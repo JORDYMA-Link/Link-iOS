49ef2bd9a8b1d43245f8bfd855931851c84e436f
김규철 <rbcjf122@gmail.com>
2024-04-02 21:26:55 +0900

    [CHORE] 디자인시스템 Color, Icon 추가 (#7)

--- Modules/App/Sources/Features/Home/View/HomeView.swift
+++ Modules/App/Sources/Features/Home/View/HomeView.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 import Combine
+import CommonFeature
 import SwiftUIIntrospect
 
 public struct HomeView: View {
@@ -17,7 +18,11 @@ public struct HomeView: View {
     public init() {}
     
     public var body: some View {
-        Text("Hello, World!")
+        Text("비트코인 2억간다.")
+            .foregroundColor(Color.bkColor(.red))
+        Text("비트코인 3억간다.")
+            .foregroundColor(Color.bkColor(.main700))
+            .font(.semiBold(size: BKFont.DisplaySize.Display1))
             .onAppear {
                 viewModel.loadCoinData()
             }
