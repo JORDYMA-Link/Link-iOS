0a38cd91005ff849a5cec45b673034604a2a5cfb
김규철 <rbcjf122@gmail.com>
2024-04-08 16:45:30 +0900

    [ADD] BKTabViewModel (#7)

---
+++ Modules/App/Sources/Features/TabBar/ViewModel/BKTabViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  BKTabViewModel.swift
+//  Blink
+//
+//  Created by 김규철 on 2024/04/08.
+//  Copyright © 2024 jordyma. All rights reserved.
+//
+
+import SwiftUI
+
+final class BKTabViewModel: ObservableObject {
+    @Published var currentItem: TabViewType = .home
+    
+    var view: some View {
+        return currentItem.view
+    }
+    
+}
