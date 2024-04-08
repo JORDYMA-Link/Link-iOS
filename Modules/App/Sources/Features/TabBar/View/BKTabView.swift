d0e02d5933e44b1bc55bb4478dea7180117eed6a
김규철 <rbcjf122@gmail.com>
2024-04-08 16:44:56 +0900

    [ADD] BKTabView 구현 (#7)

---
+++ Modules/App/Sources/Features/TabBar/View/BKTabView.swift
@@ -0,0 +1,83 @@
+//
+//  BKTabView.swift
+//  Blink
+//
+//  Created by 김규철 on 2024/04/07.
+//  Copyright © 2024 jordyma. All rights reserved.
+//
+
+import SwiftUI
+
+import CommonFeature
+
+struct BKTabView: View {
+    @State private var showMenu = false
+    @ObservedObject var viewModel = BKTabViewModel()
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            VStack(spacing: 0) {
+                
+                Spacer(minLength: 0)
+                
+                ZStack {
+                    viewModel.view
+                    
+                    if showMenu {
+                        Color.bkColor(.black).opacity(0.6)
+                            .edgesIgnoringSafeArea(.all)
+                            .onTapGesture {
+                                withAnimation {
+                                    showMenu = false
+                                }
+                            }
+                    }
+                }
+                
+                Spacer(minLength: 0)
+                
+                HStack {
+                    Spacer()
+                    
+                    VStack {
+                        TabIcon(tabViewType: .home, viewModel: viewModel, showMenu: $showMenu)
+                            .padding(.top, 15)
+                        
+                        Spacer()
+                    }
+                    
+                    TabCircleIcon(showMenu: $showMenu)
+                        .onTapGesture {
+                            withAnimation {
+                                showMenu.toggle()
+                            }
+                        }
+                    
+                    VStack {
+                        TabIcon(tabViewType: .folder, viewModel: viewModel, showMenu: $showMenu)
+                            .padding(.top, 15)
+                        
+                        Spacer()
+                    }
+                    
+                    Spacer()
+                }
+                .frame(height: 82)
+                .frame(maxWidth: .infinity)
+                .background(Color.bkColor(.white))
+            }
+            
+            if showMenu {
+                PopUpMenu()
+                    .padding(.bottom, 144)
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        BKTabView()
+    }
+}
