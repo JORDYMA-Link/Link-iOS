65a72e8b6dc9b8ddd1c19500b420b06348201f79
김규철 <rbcjf122@gmail.com>
2024-04-12 18:22:57 +0900

    [ADD] ContentDetail 추가 (#9)

---
+++ Modules/App/Sources/Features/Home/ContentDetail/View/ContentDetailView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentDetailView.swift
+//  Blink
+//
+//  Created by 김규철 on 2024/04/09.
+//  Copyright © 2024 jordyma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
