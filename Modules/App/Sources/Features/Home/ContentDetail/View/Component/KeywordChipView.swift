2e08370106e98a2dba95d5c318ce534334a3b7d3
김규철 <rbcjf122@gmail.com>
2024-04-22 00:28:40 +0900

    [ADD] KeywordChipView (#9)

---
+++ Modules/App/Sources/Features/Home/ContentDetail/View/Component/KeywordChipView.swift
@@ -0,0 +1,36 @@
+//
+//  KeywordChipView.swift
+//  Blink
+//
+//  Created by 김규철 on 2024/04/21.
+//  Copyright © 2024 jordyma. All rights reserved.
+//
+
+import SwiftUI
+
+import CommonFeature
+
+struct KeywordChipView: View {
+    var body: some View {
+        ScrollView(.horizontal) {
+            LazyHStack {
+                keywordText
+                keywordText
+                keywordText
+            }
+        }
+    }
+    
+    private var keywordText: some View {
+        Text("Design System")
+            .font(.semiBold(size: BKFont.CaptionSize.Caption3))
+            .foregroundColor(.bkColor(.gray700))
+            .padding(.vertical, 5)
+            .padding(.horizontal, 9)
+            .overlay(
+                RoundedRectangle(cornerRadius: 16)
+                    .stroke(Color.bkColor(.gray500), lineWidth: 1)
+            )
+    }
+    
+}
