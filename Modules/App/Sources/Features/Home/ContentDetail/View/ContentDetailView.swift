7819da50d64624b42b5b6f9a1f7aecbe5d5ad751
김규철 <rbcjf122@gmail.com>
2024-04-22 00:29:56 +0900

    [CHORE] 콘텐츠디테일뷰 UI만 구현 (#9)

--- Modules/App/Sources/Features/Home/ContentDetail/View/ContentDetailView.swift
+++ Modules/App/Sources/Features/Home/ContentDetail/View/ContentDetailView.swift
@@ -11,30 +11,92 @@ import SwiftUI
 import CommonFeature
 
 struct ContentDetailView: View {
-    @State private var showEdit = false
+    @ObservedObject private var viewModel = ContentDetailViewModel()
     
     var body: some View {
-        ScrollView {
-            ZStack(alignment: .top) {
-                CommonFeatureAsset.Images.contentDetailBackground.swiftUIImage
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: UIScreen.main.bounds.width, height: 304)
-                
-                VStack {
-                    cotentDetailNavigationBar
-                    cotentDetailTopView
-                        .padding(.top, 32)
+        ZStack(alignment: .bottom) {
+            ScrollView {
+                VStack(spacing: 0) {
+                    ZStack(alignment: .top) {
+                        CommonFeatureAsset.Images.contentDetailBackground.swiftUIImage
+                            .resizable()
+                            .frame(height: 272)
+                        
+                        VStack(spacing: 0) {
+                            cotentDetailNavigationBar
+                            topContentView
+                            topButtonView
+                        }
+                        .padding(.horizontal, 16)
+                    }
+                    .padding(.bottom, 24)
+                    
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("요약 내용")
+                            .font(.semiBold(size: BKFont.TitileSize.Titile3))
+                            .foregroundColor(Color.bkColor(.black))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        ContentDetailTextView(text: viewModel.dummyText1)
+                        KeywordChipView()
+                    }
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 16)
+                    
+                    VStack(alignment: .leading, spacing: 8) {
+                        HStack(spacing: 6) {
+                            Text("폴더")
+                                .font(.semiBold(size: BKFont.TitileSize.Titile3))
+                                .foregroundColor(Color.bkColor(.black))
+                            Text("수정")
+                                .font(.regular(size: BKFont.BodySize.Body4))
+                                .foregroundColor(Color.bkColor(.gray600))
+                            Spacer()
+                        }
+                        FolderItemView(text: "기획")
+                    }
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 16)
+                    
+                    VStack(alignment: .leading, spacing: 8) {
+                        HStack(spacing: 6) {
+                            Text("메모")
+                                .font(.semiBold(size: BKFont.TitileSize.Titile3))
+                                .foregroundColor(Color.bkColor(.black))
+                            Text("수정")
+                                .font(.regular(size: BKFont.BodySize.Body4))
+                                .foregroundColor(Color.bkColor(.gray600))
+                            Spacer()
+                        }
+                        ContentDetailTextView(text: viewModel.dummyText2)
+                    }
+                    .padding(.horizontal, 16)
                 }
             }
+            .frame(maxWidth: .infinity)
+            .toolbar(.hidden, for: .navigationBar)
+            .edgesIgnoringSafeArea(.top)
+            
+            Button {
+                print("원문보기")
+            } label: {
+                Text("원문보기")
+                    .font(.semiBold(size: BKFont.BtntxtSize.Btntxt1))
+                    .foregroundColor(.bkColor(.white))
+                    .padding(.vertical, 14)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 52)
+                    .background(Color.bkColor(.main300))
+                    .cornerRadius(10)
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 14)
+            .background(.white)
         }
-        .frame(maxWidth: .infinity)
-        .toolbar(.hidden, for: .navigationBar)
-        .edgesIgnoringSafeArea(.top)
     }
     
     private var cotentDetailNavigationBar: some View {
-        HStack {
+        HStack(spacing: 0) {
             Button {
                 print("icoChevronLeft")
             } label: {
@@ -42,7 +104,6 @@ struct ContentDetailView: View {
                     .renderingMode(.template)
                     .foregroundColor(Color.bkColor(.white))
             }
-            .padding(.leading, 16)
             
             Spacer()
             
@@ -53,35 +114,70 @@ struct ContentDetailView: View {
                     .renderingMode(.template)
                     .foregroundColor(Color.bkColor(.white))
             }
-            .padding(.trailing, 16)
         }
         .frame(height: 56)
         .padding(.top, UIApplication.topSafeAreaInset)
     }
     
-    private var cotentDetailTopView: some View {
+    private var topContentView: some View {
         VStack(alignment: .leading, spacing: 0) {
             CommonFeatureAsset.Images.icoBellClick.swiftUIImage
                 .padding(.bottom , 4)
+                .frame(maxWidth: .infinity, alignment: .leading)
             
             Text("방문자 상위 50위 생성형 AI 웹 서비스 분석")
-                .foregroundColor(Color.bkColor(.white))
                 .font(.regular(size: BKFont.DisplaySize.Display6))
+                .foregroundColor(Color.bkColor(.white))
                 .lineLimit(2)
             
             Text("2024.02.12")
-                .foregroundColor(Color.bkColor(.white))
                 .font(.regular(size: BKFont.BtntxtSize.Btntxt1))
-            
+                .foregroundColor(Color.bkColor(.white))
         }
     }
     
-    @ViewBuilder
-    private var editView: some View {
-        Button {
-            showEdit.toggle()
-        } label: {
-            CommonFeatureAsset.Images.icoImg.swiftUIImage
+    private var topButtonView: some View {
+        HStack(spacing: 20) {
+            Button {
+                print("icoBell")
+            } label: {
+                CommonFeatureAsset.Images.icoBell.swiftUIImage
+                    .renderingMode(.template)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 20, height: 20)
+                    .foregroundColor(Color.bkColor(.white))
+            }
+            
+            Button {
+                print("icoSave")
+            } label: {
+                CommonFeatureAsset.Images.icoSave.swiftUIImage
+                    .renderingMode(.template)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 20, height: 20)
+                    .foregroundColor(Color.bkColor(.white))
+            }
+            
+            Button {
+                print("icoShare")
+            } label: {
+                CommonFeatureAsset.Images.icoShare.swiftUIImage
+                    .renderingMode(.template)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 20, height: 20)
+                    .foregroundColor(Color.bkColor(.white))
+            }
         }
+        .frame(maxWidth:.infinity, alignment: .trailing)
+        .padding(.bottom, 24)
+    }
+}
+
+struct ContentDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentDetailView()
     }
 }
