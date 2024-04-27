7e9d71d2b8dea5b8d42f75c5d7088e27138bd569
J.H. Moon <moonjh587@naver.com>
2024-04-28 03:13:16 +0900

    [CHORE] 디자인 시스템: Modal 열거형을 통한 캡슐화 구현

--- Modules/CommonFeature/Sources/DesignSystem/BKModal.swift
+++ Modules/CommonFeature/Sources/DesignSystem/BKModal.swift
@@ -12,14 +12,11 @@ import Lottie
 ///공통으로 사용되는 Modal
 public struct BKModal: View {
     //MARK: - Properties
-    private var cancleAction: (() -> Void)?
-    private var checkAction: (() -> Void)?
-    
+    private let modalType: BKModalType
     
     //MARK: - Initialization
-    public init(cancleAction: (() -> Void)? = nil, checkAction: ( () -> Void)? = nil) {
-        self.cancleAction = cancleAction
-        self.checkAction = checkAction
+    public init(modalType: BKModalType) {
+        self.modalType = modalType
     }
     
     //MARK: - body
@@ -33,51 +30,61 @@ public struct BKModal: View {
     //MARK: - modalView
     @ViewBuilder
     public var modalView: some View {
-        ZStack{
-            Rectangle()
-                .frame(maxWidth: 327, maxHeight: 307)
-                .clipShape(.rect(cornerRadius: 10))
-                .foregroundStyle(.white)
-            
-            VStack{
+        VStack{
+            if case .linkLoading(_, _) = modalType {
                 LottieView(animation: .named("lodingAnimation", bundle: CommonFeatureResources.bundle))
                     .playing(loopMode: .loop)
                     .frame(width: 90, height: 59)
                     .padding(EdgeInsets(top: 0, leading: 0, bottom: 8, trailing: 0))
+            }
+
+            Text(modalType.modalTitle)
+                .font(.semiBold(size: BKFont.TitileSize.Titile4))
+                .padding(.bottom, 8)
+            
+            Text(modalType.modalDescription)
+                .font(.regular(size: BKFont.BodySize.Body2))
+                .multilineTextAlignment(.center)
+                .foregroundStyle(BKColor.gray700.swiftUIColor)
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 16, trailing: 0))
+            
+            
+            switch modalType {
+            case .linkLoading(let checkAction, let cancelAction), .cancelConfirm(checkAction: let checkAction, cancelAction: let cancelAction), .deleteFolder(let checkAction, let cancelAction), .deleteContent(let checkAction, let cancelAction):
                 
-                Text("기본 모달 케이스")
-                    .font(.title3)
-                    .padding(.bottom, 8)
+                configureButton(checkAction: checkAction, cancelAction: cancelAction)
                 
-                Text("여기에 텍스트를 적습니다.\n한줄 두줄 모두 가능")
-                    .font(.caption)
-                    .multilineTextAlignment(.center)
-                    .foregroundStyle(BKColor.gray700.swiftUIColor)
-                
-                if let checkAction {
-                    HStack(content: {
-                        if let cancleAction {
-                            Button(action: cancleAction, label: {
-                                Text("취소")
-                                    .foregroundStyle(BKColor.gray700.swiftUIColor)
-                            })
-                            .frame(maxWidth: 140, maxHeight: 48)
-                            .background(BKColor.white.swiftUIColor)
-                            .border(BKColor.gray500.swiftUIColor, width: 1)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                        }
-                        
-                        Button(action: checkAction, label: {
-                            Text("확인")
-                                .foregroundStyle(BKColor.white.swiftUIColor)
-                        })
-                        .frame(maxWidth: 140, maxHeight: 48)
-                        .background(BKColor.gray900.swiftUIColor)
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                    })
-                    .padding(EdgeInsets(top: 16, leading: 0, bottom: 0, trailing: 0))
-                }
+            case .custom(_, _, let checkAction, let cancelAction):
+                configureButton(checkAction: checkAction, cancelAction: cancelAction)
             }
         }
+        .padding(EdgeInsets(top: 28, leading: 20, bottom: 28, trailing: 20))
+        .background(RoundedRectangle(cornerRadius: 10).fill(BKColor.white.swiftUIColor))
+    }
+    
+    //MARK: - makeButton
+    @ViewBuilder
+    private func configureButton(checkAction: @escaping ()->Void, cancelAction: (()->Void)?) -> some View {
+        HStack(content: {
+            if let cancelAction{
+                Button(action: cancelAction, label: {
+                    Text("취소")
+                        .foregroundStyle(BKColor.gray700.swiftUIColor)
+                })
+                .frame(maxWidth: 140, maxHeight: 48)
+                .background(BKColor.white.swiftUIColor)
+                .border(BKColor.gray500.swiftUIColor, width: 1)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
+            
+            Button(action: checkAction, label: {
+                Text("확인")
+                    .foregroundStyle(BKColor.white.swiftUIColor)
+            })
+            .frame(maxWidth: 140, maxHeight: 48)
+            .background(BKColor.gray900.swiftUIColor)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+        })
     }
 }
+
