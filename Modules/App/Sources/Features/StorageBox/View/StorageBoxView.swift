b3eb96e27de79af7a922506b57694cd6e296e57c
kimkyuchul <rbcjf122@gmail.com>
2024-04-27 18:28:50 +0900

    [ADD] StorageBoxView (#12)

---
+++ Modules/App/Sources/Features/StorageBox/View/StorageBoxView.swift
@@ -0,0 +1,19 @@
+//
+//  StorageBoxView.swift
+//  Blink
+//
+//  Created by kyuchul on 4/27/24.
+//  Copyright © 2024 jordyma. All rights reserved.
+//
+
+import SwiftUI
+
+struct StorageBoxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StorageBoxView()
+}
