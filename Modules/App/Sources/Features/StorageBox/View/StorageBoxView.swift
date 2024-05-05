ccc2a29956b34fd4812507c911d03a89f4ca375b
김규철 <rbcjf122@gmail.com>
2024-05-05 19:22:38 +0900

    [CHORE] StorageBoxView 폴더추가, 폴더 Cell, 폴더 상세 이동 (#16)

--- Modules/App/Sources/Features/StorageBox/View/StorageBoxView.swift
+++ Modules/App/Sources/Features/StorageBox/View/StorageBoxView.swift
@@ -8,10 +8,11 @@
 
 import SwiftUI
 
+import CommonFeature
+
 struct StorageBoxView: View {
     @State private var contentText: String = ""
-    
-    let columns: [GridItem] = [GridItem(.flexible(), spacing: 14), GridItem(.flexible())]
+    @State private var pushToContentList = false
     
     var body: some View {
         VStack {
@@ -21,15 +22,23 @@ struct StorageBoxView: View {
                 .padding(EdgeInsets(top: 8, leading: 16, bottom: 24, trailing: 16))
             
             ScrollView(showsIndicators: false) {
-                LazyVGrid(columns: columns, spacing: 16) {
-                    Rectangle()
-                        .frame(height: 80)
-                        .foregroundColor(Color.red)
+                LazyVGrid(columns: [GridItem(.flexible(), spacing: 14), GridItem(.flexible())], spacing: 16) {
+                    AddStorageBoxCell()
+                        .onTapGesture {
+                            print("폴더 추가하기")
+                        }
                     
                     ForEach(1..<20) { index in
-                        Rectangle()
-                            .frame(height: 80)
-                            .foregroundColor(Color.blue)
+                        StorageBoxCell(
+                            folderCount: 90,
+                            folderName: "할리스커피",
+                            menuAction: {
+                                print("설정 바텀시트")
+                            }
+                        )
+                        .onTapGesture {
+                            pushToContentList.toggle()
+                        }
                     }
                 }
                 .padding(.vertical, 32)
@@ -38,10 +47,77 @@ struct StorageBoxView: View {
             .background(Color.bkColor(.gray300))
         }
         .background(Color.bkColor(.white))
+        .navigationDestination(isPresented: $pushToContentList) {
+            StorageBoxContentListView()
+        }
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
                 LeadingItem(type: .tab("보관함"))
             }
+        }
+    }
+    
+    private struct AddStorageBoxCell: View {
+        var body: some View {
+            ZStack {
+                Color(.bkColor(.white))
+                
+                VStack(alignment: .center, spacing: 4) {
+                    CommonFeatureAsset.Images.icoPlus.swiftUIImage
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .foregroundStyle(Color.bkColor(.gray700))
+                    
+                    Text("추가하기")
+                        .font(.regular(size: ._14))
+                        .foregroundStyle(Color.bkColor(.gray700))
+                }
+                .padding(EdgeInsets(top: 16, leading: 14, bottom: 18, trailing: 14))
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .frame(height: 80)
+            .shadow(color: .bkColor(.gray900).opacity(0.08), radius: 5, x: 0, y: 4)
+        }
+    }
+    
+    private struct StorageBoxCell: View {
+        var folderCount: Int
+        var folderName: String
+        var menuAction: () -> Void
+        
+        var body: some View {
+            ZStack {
+                Color(.bkColor(.white))
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        Text("\(folderCount)개")
+                            .font(.regular(size: ._12))
+                            .foregroundStyle(Color.bkColor(.gray800))
+                            .lineLimit(1)
+                        
+                        Spacer(minLength: 4)
+                        
+                        Button(action: {
+                            menuAction()
+                        }, label: {
+                            CommonFeatureAsset.Images.icoMoreVertical.swiftUIImage
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .foregroundStyle(Color.bkColor(.gray600))
+                        })
+                    }
+                    
+                    Text(folderName)
+                        .font(.semiBold(size: ._14))
+                        .foregroundStyle(Color.bkColor(.gray900))
+                        .lineLimit(1)
+                }
+                .padding(EdgeInsets(top: 16, leading: 14, bottom: 18, trailing: 14))
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .frame(height: 80)
+            .shadow(color: .bkColor(.gray900).opacity(0.08), radius: 5, x: 0, y: 4)
         }
     }
 }
