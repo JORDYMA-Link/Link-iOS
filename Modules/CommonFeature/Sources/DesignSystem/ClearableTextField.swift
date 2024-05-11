9e7e496d032bb0225f1ad68444889d59b0a61a7c
J.H. Moon <moonjh587@naver.com>
2024-05-11 23:42:32 +0900

    [ADD] http, https 정규식추가, clearButton 포함 텍스트 필드 생성

---
+++ Modules/CommonFeature/Sources/DesignSystem/ClearableTextField.swift
@@ -0,0 +1,19 @@
+//
+//  ClearableTextField.swift
+//  CommonFeature
+//
+//  Created by 문정호 on 5/10/24.
+//  Copyright © 2024 jordyma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ClearableTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClearableTextField()
+}
