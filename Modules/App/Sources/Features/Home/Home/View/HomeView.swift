e84800e110cc8a80a4e9271741f69e028bb27832
kimkyuchul <rbcjf122@gmail.com>
2024-05-17 17:06:30 +0900

    #19:
    [FIX] 홈뷰 코드 정리 (#19)

--- Modules/App/Sources/Features/Home/Home/View/HomeView.swift
+++ Modules/App/Sources/Features/Home/Home/View/HomeView.swift
@@ -23,53 +23,23 @@ struct HomeView: View {
     var body: some View {
         GeometryReader { geometry in
             VStack(spacing: 0) {
-                makeBKNavigationView(leadingType: .home, trailingType: .oneIcon(action: {
-                    pushToSetting.toggle()
-                }, icon: CommonFeatureAsset.Images.icoSettings.swiftUIImage), tintColor: .bkColor(.gray900))
-                .padding(.horizontal, 16)
+                makeNavigationView()
                 
                 ScrollView(showsIndicators: false) {
                     VStack(spacing: 12) {
-                        useBlinkBanner
-                        calendarBanner
+                        makeUseBlinkBanner()
+                        makeCalendarBanner()
                     }
                     .padding(.init(top: 8, leading: 16, bottom: 24, trailing: 16))
                     
-                    ZStack {
-                        Color.bkColor(.gray300)
-                        
-                        LazyVStack(spacing: 4, pinnedViews: [.sectionHeaders]) {
-                            Section {
-                                ForEach(1...10, id: \.self) { count in
-                                    BKCardCell(width: geometry.size.width - 32, sourceTitle: "브런치", sourceImage: CommonFeatureAsset.Images.graphicBell.swiftUIImage, saveAction: {}, menuAction: {}, title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"])
-                                        .padding(.init(top: 0, leading: 16, bottom: 16, trailing: 16))
-                                }
-                            } header: {
-                                CategoryHorizontalList(selectedIndex: $categorySelectedIndex)
-                                    .background(GeometryReader { proxy in
-                                        Color.clear.preference(key: HeaderMaxYPreferenceKey.self, value: proxy.frame(in: .global).maxY)
-                                    })
-                                    .onPreferenceChange(HeaderMaxYPreferenceKey.self) { maxY in
-                                        // 섹션 헤더의 최대 Y 위치 업데이트
-                                        let navigationBarMaxY = (geometry.safeAreaInsets.top - 20)
-                                        let headerMaxY = maxY + navigationBarMaxY
-                                        
-                                        DispatchQueue.main.async {
-                                            scrollViewDelegate.headerMaxY = headerMaxY
-                                        }
-                                    }
-                                    .background(topToCategory ? Color.white : Color.bkColor(.gray300))
-                                    .clipped()
-                                    .shadow(color: topToCategory ? .bkColor(.gray900).opacity(0.08) : .clear, radius: 5, x: 0, y: 4)
-                            }
-                        }
-                    }
+                    makeArticleListView(geometry)
+                }
+                .introspect(.scrollView, on: .iOS(.v16, .v17)) { scrollView in
+                    scrollView.delegate = scrollViewDelegate
                 }
             }
-            .introspect(.scrollView, on: .iOS(.v16, .v17)) { scrollView in
-                    scrollView.delegate = scrollViewDelegate
-            }
         }
+        .toolbar(.hidden, for: .navigationBar)
         .onAppear {
             viewModel.loadCoinData()
         }
@@ -81,10 +51,20 @@ struct HomeView: View {
         .navigationDestination(isPresented: $pushToSetting) {
             SettingView()
         }
-        .toolbar(.hidden, for: .navigationBar)
+    }
+}
+
+extension HomeView {
+    @ViewBuilder
+    private func makeNavigationView() -> some View {
+        makeBKNavigationView(leadingType: .home, trailingType: .oneIcon(action: {
+            pushToSetting.toggle()
+        }, icon: CommonFeatureAsset.Images.icoSettings.swiftUIImage), tintColor: .bkColor(.gray900))
+        .padding(.horizontal, 16)
     }
     
-    private var useBlinkBanner: some View {
+    @ViewBuilder
+    private func makeUseBlinkBanner() -> some View {
         HStack(spacing: 0) {
             ZStack {
                 Circle()
@@ -123,7 +103,8 @@ struct HomeView: View {
         .clipShape(RoundedRectangle(cornerRadius: 10))
     }
     
-    private var calendarBanner: some View {
+    @ViewBuilder
+    private func makeCalendarBanner() -> some View {
         ZStack {
             Color.bkColor(.gray300)
             
@@ -152,42 +133,83 @@ struct HomeView: View {
         .clipShape(RoundedRectangle(cornerRadius: 10))
     }
     
-    private struct CategoryHorizontalList: View {
-        @Binding var selectedIndex: Int?
-        var categories = ["중요", "미분류"]
-        
-        var body: some View {
-            ScrollView(.horizontal) {
-                HStack(spacing: 8) {
-                    ForEach(categories.indices, id: \.self) { index in
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 100)
-                                .fill(selectedIndex == index ? Color.black : Color.white)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 100)
-                                        .stroke(selectedIndex == index ? Color.clear : Color.bkColor(.gray500), lineWidth: 1)
-                                )
+    @ViewBuilder
+    private func makeArticleListView(_ geometry: GeometryProxy) -> some View {
+        ZStack {
+            Color.bkColor(.gray300)
+            
+            LazyVStack(spacing: 4, pinnedViews: [.sectionHeaders]) {
+                Section {
+                    ForEach(1...10, id: \.self) { count in
+                        BKCardCell(width: geometry.size.width - 32, sourceTitle: "브런치", sourceImage: CommonFeatureAsset.Images.graphicBell.swiftUIImage, saveAction: {}, menuAction: {}, title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"])
+                            .padding(.init(top: 0, leading: 16, bottom: 16, trailing: 16))
+                    }
+                } header: {
+                    makeCategorySectionHeader(selectedIndex: $categorySelectedIndex)
+                        .background(GeometryReader { proxy in
+                            Color.clear.preference(key: CategorySectionHeaderPreferenceKey.self, value: proxy.frame(in: .global).maxY)
+                        })
+                        .onPreferenceChange(CategorySectionHeaderPreferenceKey.self) { maxY in
+                            // 섹션 헤더의 최대 Y 위치 업데이트
+                            let navigationBarMaxY = (geometry.safeAreaInsets.top - 20)
+                            let headerMaxY = maxY + navigationBarMaxY
                             
-                            Text(categories[index])
-                                .font(selectedIndex == index ? .semiBold(size: ._14) : .regular(size: ._14))
-                                .foregroundColor(selectedIndex == index ? Color.bkColor(.white) : Color.bkColor(.black))
-                                .padding(.vertical, 10)
-                                .padding(.horizontal, 14)
-                        }
-                        .onTapGesture {
-                            if selectedIndex == index {
-                                selectedIndex = nil
-                            } else {
-                                selectedIndex = index
+                            DispatchQueue.main.async {
+                                scrollViewDelegate.headerMaxY = headerMaxY
                             }
                         }
-                    }
+                        .background(topToCategory ? Color.white : Color.bkColor(.gray300))
+                        .clipped()
+                        .shadow(color: topToCategory ? .bkColor(.gray900).opacity(0.08) : .clear, radius: 5, x: 0, y: 4)
                 }
-                .padding(.leading, 16)
             }
-            .scrollDisabled(true)
-            .padding(.top, 8)
-            .padding(.bottom, 12)
+        }
+    }
+    
+    @ViewBuilder
+    private func makeCategorySectionHeader(selectedIndex: Binding<Int?>) -> some View {
+        var categories = ["중요", "미분류"]
+        
+        ScrollView(.horizontal) {
+            HStack(spacing: 8) {
+                ForEach(categories.indices, id: \.self) { index in
+                    Text(categories[index])
+                        .font(selectedIndex.wrappedValue == index ? .semiBold(size: ._14) : .regular(size: ._14))
+                        .foregroundColor(selectedIndex.wrappedValue == index ? Color.white : Color.black)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 14)
+                        .background(
+                            RoundedRectangle(cornerRadius: 100)
+                                .fill(selectedIndex.wrappedValue == index ? Color.black : Color.white)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 100)
+                                        .stroke(selectedIndex.wrappedValue == index ? Color.clear : Color.bkColor(.gray500), lineWidth: 1)
+                                )
+                            
+                        )
+                        .onTapGesture {
+                            if selectedIndex.wrappedValue == index {
+                                selectedIndex.wrappedValue = nil
+                            } else {
+                                selectedIndex.wrappedValue = index
+                            }
+                        }
+                }
+            }
+            .padding(.leading, 16)
+        }
+        .scrollDisabled(true)
+        .padding(.top, 8)
+        .padding(.bottom, 12)
+    }
+}
+
+extension HomeView {
+    private struct CategorySectionHeaderPreferenceKey: PreferenceKey {
+        static var defaultValue: CGFloat = .zero
+        
+        static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+            value = max(value, nextValue())
         }
     }
 }
@@ -199,29 +221,18 @@ struct HomeView: View {
 
 @MainActor
 final class HomeScrollViewDelegate: NSObject, UIScrollViewDelegate, ObservableObject {
-    @Published var headerBackground: Color = .clear
     @Published var headerMaxY: CGFloat = .zero
     @Published var topToCategory: Bool = false
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         if scrollView.contentOffset.y > headerMaxY {
             DispatchQueue.main.async {
-                self.headerBackground = .white
                 self.topToCategory = true
             }
         } else {
             DispatchQueue.main.async {
-                self.headerBackground = .clear
                 self.topToCategory = false
             }
         }
-    }
-}
-
-struct HeaderMaxYPreferenceKey: PreferenceKey {
-    static var defaultValue: CGFloat = .zero
-    
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        value = max(value, nextValue())
     }
 }
