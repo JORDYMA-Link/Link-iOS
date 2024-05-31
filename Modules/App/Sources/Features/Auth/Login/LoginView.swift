5b3800989f5374fbd208614dd0a601a82bd9c60c
kimkyuchul <rbcjf122@gmail.com>
2024-05-31 14:04:07 +0900

    [SETTING] 로고, 로고텍스트, 카카오 애플 아이콘 추가 (#23)

---
+++ Modules/App/Sources/Features/Auth/Login/LoginView.swift
@@ -0,0 +1,19 @@
+//
+//  LoginView.swift
+//  Blink
+//
+//  Created by kyuchul on 5/31/24.
+//  Copyright © 2024 jordyma. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
