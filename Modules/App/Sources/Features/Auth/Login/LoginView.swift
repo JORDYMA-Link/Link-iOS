ad6f9439ee71932f09d8a6bb3334d73f590baf61
kimkyuchul <rbcjf122@gmail.com>
2024-05-31 14:17:38 +0900

    [CHORE] 로그인뷰 로고 추가 (#23)

--- Modules/App/Sources/Features/Auth/Login/LoginView.swift
+++ Modules/App/Sources/Features/Auth/Login/LoginView.swift
@@ -8,9 +8,23 @@
 
 import SwiftUI
 
+import CommonFeature
+
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.white.ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color.bkColor(.main300))
+                        .frame(width: 135, height: 135)
+                    
+                    BKIcon(image: CommonFeatureAsset.Images.logoWhite.swiftUIImage, color: .white, size: CGSize(width: 64, height: 70))
+                }
+            }
+        }
     }
 }
 
