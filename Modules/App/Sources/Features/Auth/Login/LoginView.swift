32630547b8181c3c14df85c12d8fd1db5361f747
kimkyuchul <rbcjf122@gmail.com>
2024-06-06 14:56:35 +0900

    [FEATURE] 로그인 & 온보딩 뷰 레이아웃 2차 구현 (#23)

--- Modules/App/Sources/Features/Auth/Login/LoginView.swift
+++ Modules/App/Sources/Features/Auth/Login/LoginView.swift
@@ -11,27 +11,7 @@ import SwiftUI
 import CommonFeature
 
 struct LoginView: View {
-    @Environment(\.openURL) var openURL
-    
     @State private var pushToOnboarding = false
-    private var attributedString: AttributedString {
-        let string = "서비스 약관"
-        var attributedString = AttributedString(string)
-        attributedString.foregroundColor = .bkColor(.gray600)
-        attributedString.font = .regular(size: ._12)
-        attributedString.underlineStyle = .single
-        attributedString.link = URL(string: "https://www.naver.com")
-        return attributedString
-    }
-    private var aattributedString: AttributedString {
-        let string = "개인정보 처리방침"
-        var attributedString = AttributedString(string)
-        attributedString.foregroundColor = .bkColor(.gray600)
-        attributedString.font = .regular(size: ._12)
-        attributedString.underlineStyle = .single
-        attributedString.link = URL(string: "https://www.kakao.com")
-        return attributedString
-    }
     
     var body: some View {
         NavigationStack {
@@ -41,80 +21,24 @@ struct LoginView: View {
                 VStack(alignment: .center, spacing: 0) {
                     Spacer()
                     
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 25, style: .continuous)
-                            .fill(Color.bkColor(.main300))
-                            .frame(width: 135, height: 135)
-                        
-                        BKIcon(image: CommonFeatureAsset.Images.logoWhite.swiftUIImage, color: .white, size: CGSize(width: 64, height: 70))
-                    }
-                    
-                    Text("눈 깜짝할 새 저장되는\nAI 링크 아카이빙, 블링크")
-                        .font(.semiBold(size: ._20))
-                        .foregroundStyle(Color.bkColor(.gray900))
-                        .multilineTextAlignment(.center)
-                        .padding(.top, 16)
-                        .frame(alignment: .center)
+                    makeLogo()
+                    makeTitle()
                     
                     Spacer()
                     
                     VStack(spacing: 12) {
-                        Button {
+                        makeLoginButton(action: {
                             pushToOnboarding = true
-                        } label: {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 10, style: .continuous)
-                                    .fill(Color.bkColor(.kakaoYellow))
-                                    .frame(maxWidth: .infinity)
-                                
-                                HStack(spacing: 4) {
-                                    BKIcon(image: CommonFeatureAsset.Images.icokakao.swiftUIImage, color: .bkColor(.gray900), size: CGSize(width: 20, height: 20))
-                                    
-                                    Text("카카오톡으로 시작하기")
-                                        .font(.semiBold(size: ._16))
-                                        .foregroundStyle(Color.bkColor(.gray900))
-                                        .multilineTextAlignment(.center)
-                                }
-                                .padding(EdgeInsets(top: 14, leading: 16, bottom: 14, trailing: 16))
-                            }
-                            .padding(.top)
-                            .padding(.horizontal, 16)
-                            .frame(height: 48)
-                        }
+                        }, backgroundColor: .bkColor(.kakaoYellow), title: "카카오톡으로 시작하기", titleColor: .bkColor(.gray900), buttonImage: CommonFeatureAsset.Images.icokakao.swiftUIImage, buttonImageColor: .bkColor(.gray900))
                         
-                        Button {
-                            print("애플로 시작")
-                        } label: {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 10, style: .continuous)
-                                    .fill(Color.bkColor(.black))
-                                    .frame(maxWidth: .infinity)
-                                
-                                HStack(spacing: 4) {
-                                    BKIcon(image: CommonFeatureAsset.Images.icoapple.swiftUIImage, color: .bkColor(.white), size: CGSize(width: 20, height: 20))
-                                    
-                                    Text("Apple로 시작하기")
-                                        .font(.semiBold(size: ._16))
-                                        .foregroundStyle(Color.bkColor(.white))
-                                        .multilineTextAlignment(.center)
-                                }
-                                .padding(EdgeInsets(top: 14, leading: 16, bottom: 14, trailing: 16))
-                            }
-                            .padding(.top)
-                            .padding(.horizontal, 16)
-                            .frame(height: 48)
-                        }
+                        makeLoginButton(action: {
+                            pushToOnboarding = true
+                        }, backgroundColor: .bkColor(.black), title: "Apple로 시작하기", titleColor: .bkColor(.white), buttonImage: CommonFeatureAsset.Images.icoapple.swiftUIImage, buttonImageColor: .bkColor(.white))
                         
-                        Text("가입을 진행할 경우\n \(attributedString) 및 \(aattributedString)에 동의한 것으로 간주합니다. ")
-                            .font(.regular(size: ._12))
-                            .foregroundStyle(Color.bkColor(.gray600))
-                            .padding(.top, 16)
-                            .lineLimit(2)
-                            .multilineTextAlignment(.center)
-                            .environment(\.openURL, OpenURLAction { url in
-                                print("\n---> Text: \(url)")
-                                return .systemAction
-                            })
+                        makeTerms(
+                            serviceTerms:makeTermsText("서비스 약관", url: "https://upbit.com/home"),
+                            privacyPolicy: makeTermsText("개인정보 처리방침", url: "https://www.bithumb.com/react")
+                        )
                     }
                 }
             }
@@ -123,8 +47,73 @@ struct LoginView: View {
             }
         }
     }
-}
-
-#Preview {
-    LoginView()
+    
+    @ViewBuilder
+    private func makeLogo() -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                .fill(Color.bkColor(.main300))
+                .frame(width: 135, height: 135)
+            
+            BKIcon(image: CommonFeatureAsset.Images.logoWhite.swiftUIImage, color: .white, size: CGSize(width: 64, height: 70))
+        }
+    }
+    
+    @ViewBuilder
+    private func makeTitle() -> some View {
+        Text("눈 깜짝할 새 저장되는\nAI 링크 아카이빙, 블링크")
+            .font(.semiBold(size: ._20))
+            .foregroundStyle(Color.bkColor(.gray900))
+            .multilineTextAlignment(.center)
+            .padding(.top, 16)
+            .frame(alignment: .center)
+    }
+    
+    @ViewBuilder
+    private func makeLoginButton(action: @escaping () -> (), backgroundColor: Color, title: String, titleColor: Color, buttonImage: Image, buttonImageColor: Color) -> some View {
+        Button {
+            action()
+        } label: {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                    .fill(backgroundColor)
+                    .frame(maxWidth: .infinity)
+                
+                HStack(spacing: 4) {
+                    BKIcon(image: buttonImage, color: buttonImageColor, size: CGSize(width: 20, height: 20))
+                    
+                    Text(title)
+                        .font(.semiBold(size: ._16))
+                        .foregroundStyle(titleColor)
+                        .multilineTextAlignment(.center)
+                }
+                .padding(EdgeInsets(top: 14, leading: 16, bottom: 14, trailing: 16))
+            }
+            .padding(.top)
+            .padding(.horizontal, 16)
+            .frame(height: 48)
+        }
+    }
+    
+    @ViewBuilder
+    private func makeTerms(serviceTerms: AttributedString, privacyPolicy: AttributedString) -> some View {
+        Text("가입을 진행할 경우\n \(serviceTerms) 및 \(privacyPolicy)에 동의한 것으로 간주합니다. ")
+            .font(.regular(size: ._12))
+            .foregroundStyle(Color.bkColor(.gray600))
+            .padding(.top, 16)
+            .lineLimit(2)
+            .multilineTextAlignment(.center)
+            .environment(\.openURL, OpenURLAction { url in
+                return .systemAction
+            })
+    }
+    
+    private func makeTermsText(_ text: String, url: String) -> AttributedString {
+        var attributedString = AttributedString(text)
+        attributedString.foregroundColor = .bkColor(.gray600)
+        attributedString.font = .regular(size: ._12)
+        attributedString.underlineStyle = .single
+        attributedString.link = URL(string: url)
+        return attributedString
+    }
 }
--- Modules/App/Sources/Features/Auth/Onboarding/OnboardingSubjectView.swift
+++ Modules/App/Sources/Features/Auth/Onboarding/OnboardingSubjectView.swift
@@ -11,88 +11,116 @@ import SwiftUI
 import CommonFeature
 
 struct OnboardingSubjectView: View {
-    private let items = [
-        ("😀", "경제"),
-        ("🍎", "기획"),
-        ("🚗", "개발"),
-        ("🌟", "독서"),
-        ("📚", "동물"),
-        ("🎵", "디자인"),
-        ("🏀", "아이돌"),
-        ("🏖", "여행"),
-        ("✈️", "영감"),
-        ("🎂", "옷"),
-        ("🎂", "요리"),
-        ("🎂", "재료"),
+    private let subjects = [
+        ("💰", "경제"),
+        ("🖌️", "기획"),
+        ("💻", "개발"),
+        ("📚", "독서"),
+        ("🐶", "동물"),
+        ("🖼", "디자인"),
+        ("🎀", "아이돌"),
+        ("✈️", "여행"),
+        ("💡", "영감"),
+        ("👚", "옷"),
+        ("🍽️", "요리"),
+        ("🩻", "의료"),
     ]
     
     var body: some View {
         VStack(spacing: 0) {
-            VStack(alignment: .leading, spacing: 4) {
-                Text("주로 어떤 주제를 아카이빙 하시나요?")
-                    .font(.semiBold(size: ._24))
-                    .foregroundStyle(Color.bkColor(.gray900))
-                
-                Text("3개를 선택해주시면 해당 폴더를 미리 만들어드릴게요!")
-                    .font(.semiBold(size: ._14))
-                    .foregroundStyle(Color.bkColor(.gray700))
-            }
+            makeTitle()
             
-            LazyVGrid(columns: [GridItem(.flexible(), spacing: 12), GridItem(.flexible(), spacing: 12), GridItem(.flexible())], spacing: 16) {
-                ForEach(items, id: \.1) { item in
-                    ZStack {
-                        Color(.bkColor(.gray300))
-                        
-                        VStack {
-                            Text(item.0)
-                                .font(.regular(size: ._16))
-                            Text(item.1)
-                                .font(.regular(size: ._16))
-                        }
-                        .padding(EdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16))
-                    }
-                    .frame(height: 84)
-                    .cornerRadius(10)
-                }
-            }
-            .padding(.top, 24)
+            makeSubjectGrid()
+                .padding(EdgeInsets(top: 24, leading: 12, bottom: 0, trailing: 12))
             
             Spacer()
             
-            Button {
-                print("확인")
-            } label: {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 10, style: .continuous)
-                        .fill(Color.bkColor(.gray500))
-                        .frame(maxWidth: .infinity)
-                    
-                    Text("확인")
-                        .font(.semiBold(size: ._16))
-                        .foregroundStyle(Color.bkColor(.gray600))
-                        .multilineTextAlignment(.center)
-                        .padding(EdgeInsets(top: 14, leading: 16, bottom: 14, trailing: 16))
-                }
-                .frame(height: 52)
-            }
+            makeConfirmButton()
         }
         .padding(EdgeInsets(top: 28, leading: 16, bottom: 16, trailing: 16))
         .navigationBarBackButtonHidden(true)
         .toolbar {
             ToolbarItem(placement: .topBarTrailing) {
-                Text("건너뛰기")
-                    .font(.semiBold(size: ._14))
-                    .foregroundStyle(Color.bkColor(.gray600))
+                makeToolbarItem()
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func makeToolbarItem() -> some View {
+        Text("건너뛰기")
+            .font(.semiBold(size: ._14))
+            .foregroundStyle(Color.bkColor(.gray600))
+            .onTapGesture {
+                print("go Home")
+            }
+    }
+    
+    @ViewBuilder
+    private func makeTitle() -> some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text("주로 어떤 주제를 아카이빙 하시나요?")
+                .font(.semiBold(size: ._24))
+                .foregroundStyle(Color.bkColor(.gray900))
+            
+            Text("3개를 선택해주시면 해당 폴더를 미리 만들어드릴게요!")
+                .font(.regular(size: ._14))
+                .foregroundStyle(Color.bkColor(.gray700))
+        }
+    }
+    
+    @ViewBuilder
+    private func makeSubjectGrid() -> some View {
+        let gridItem = [GridItem(.flexible(), spacing: 12), GridItem(.flexible(), spacing: 12), GridItem(.flexible())]
+        
+        LazyVGrid(columns: gridItem, spacing: 16) {
+            ForEach(subjects, id: \.1) { item in
+                makeSubjectCell(emoji: item.0, title: item.1)
                     .onTapGesture {
-                        print("go Home")
+                        print("Tap Subject")
                     }
             }
         }
     }
-}
-
-#Preview {
-    NavigationStack {
-        OnboardingSubjectView()
+    
+    @ViewBuilder
+    private func makeSubjectCell(emoji: String, title: String) -> some View {
+        let lineHeight = UIFont.regular(size: ._16).lineHeight
+        
+        ZStack {
+            Color(.bkColor(.gray300))
+            
+            VStack(spacing: 0) {
+                Text(emoji)
+                    .font(.regular(size: ._16))
+                    .padding(.vertical, (24 - lineHeight) / 2)
+                
+                Text(title)
+                    .font(.regular(size: ._16))
+                    .padding(.vertical, (24 - lineHeight) / 2)
+            }
+            .padding(EdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16))
+        }
+        .frame(height: 84)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+    
+    @ViewBuilder
+    private func makeConfirmButton() -> some View {
+        Button {
+            print("확인")
+        } label: {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                    .fill(Color.bkColor(.gray500))
+                    .frame(maxWidth: .infinity)
+                
+                Text("확인")
+                    .font(.semiBold(size: ._16))
+                    .foregroundStyle(Color.bkColor(.gray600))
+                    .padding(EdgeInsets(top: 14, leading: 16, bottom: 14, trailing: 16))
+            }
+            .frame(height: 52)
+        }
     }
 }
