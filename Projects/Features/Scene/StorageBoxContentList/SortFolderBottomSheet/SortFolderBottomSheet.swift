79ffae44e48d96e36d5f3add0df866b4d4e4cc74
kimkyuchul <rbcjf122@gmail.com>
2024-06-23 00:10:35 +0900

    [ADD] 폴더 콘텐츠 리스트 뷰 폴더 정렬 로직 (#29)

---
+++ Projects/Features/Scene/StorageBoxContentList/SortFolderBottomSheet/SortFolderBottomSheet.swift
@@ -0,0 +1,43 @@
+//
+//  SortFolderBottomSheet.swift
+//  Features
+//
+//  Created by kyuchul on 6/22/24.
+//  Copyright © 2024 com.kyuchul.blink. All rights reserved.
+//
+
+import SwiftUI
+
+import CommonFeature
+
+import ComposableArchitecture
+
+struct SortFolderBottomSheet: View {
+  @Bindable var store: StoreOf<SortFolderBottomSheetFeature>
+  
+  var body: some View {
+    HStack(spacing:0) {
+      VStack(alignment: .leading, spacing: 8) {
+        ForEach(FolderSortType.allCases, id: \.self) { type in
+          makeSortButton(title: type.rawValue, isSelected: type == store.inputSortType) {
+            store.send(.sortCellTapped(type))
+          }
+        }
+      }
+      Spacer(minLength: 0)
+    }
+  }
+  
+  @ViewBuilder
+  private func makeSortButton(title: String, isSelected: Bool = true, action: @escaping (() -> Void)) -> some View {
+    Button {
+      action()
+    } label: {
+      Text(title)
+        .font(.regular(size: ._16))
+        .foregroundStyle(Color.bkColor(isSelected ? .black : .gray600))
+        .frame(alignment: .leading)
+        .padding(.vertical, 8)
+    }
+  }
+}
