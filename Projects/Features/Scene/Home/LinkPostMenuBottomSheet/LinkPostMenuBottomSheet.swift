266c8eb6c4997d56578dd064fbcd52117fdc1993
kimkyuchul <rbcjf122@gmail.com>
2024-06-23 16:21:14 +0900

    [ADD] 홈 게시글 수정 메뉴 바텀시트 로직 구현 (#29)

---
+++ Projects/Features/Scene/Home/LinkPostMenuBottomSheet/LinkPostMenuBottomSheet.swift
@@ -0,0 +1,39 @@
+//
+//  LinkPostMenuBottomSheet.swift
+//  Features
+//
+//  Created by kyuchul on 6/23/24.
+//  Copyright © 2024 com.kyuchul.blink. All rights reserved.
+//
+
+import SwiftUI
+
+import CommonFeature
+
+import ComposableArchitecture
+
+struct LinkPostMenuBottomSheet: View {
+  @Bindable var store: StoreOf<LinkPostMenuBottomSheetFeature>
+  
+    var body: some View {
+      HStack(spacing:0) {
+        VStack(alignment: .leading, spacing: 8) {
+          makeMenuButton(title: "수정하기", action: { store.send(.menuTapped(.editLinkPost)) })
+          makeMenuButton(title: "삭제하기", isEdit: false, action: { store.send(.menuTapped(.deleteLinkPost)) })
+        }
+        Spacer(minLength: 0)
+      }
+    }
+  
+  @ViewBuilder
+  private func makeMenuButton(title: String, isEdit: Bool = true, action: @escaping (() -> Void)) -> some View {
+    Button {
+      action()
+    } label: {
+      Text(title)
+        .font(.regular(size: ._16))
+        .foregroundStyle(Color.bkColor(isEdit ? .gray900 : .red))
+        .padding(.vertical, 8)
+    }
+  }
+}
