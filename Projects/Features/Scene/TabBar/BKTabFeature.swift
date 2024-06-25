e967a913ea3739dedaa88bd701c11a34068798f5
kimkyuchul <rbcjf122@gmail.com>
2024-06-25 10:06:40 +0900

    [CHORE] TCA Action 주석 추가 (#29)

--- Projects/Features/Scene/TabBar/BKTabFeature.swift
+++ Projects/Features/Scene/TabBar/BKTabFeature.swift
@@ -16,50 +16,51 @@ public struct BKTabFeature: Reducer {
   
   @ObservableState
   public struct State: Equatable {
-        var currentItem: BKTabViewType = .home
-        var showMenu = false
-        var pushSaveLink = false
-        
-        var home: HomeFeature.State = .init()
-        var storageBox: StorageBoxFeature.State = .init()
+    var currentItem: BKTabViewType = .home
+    var showMenu = false
+    var pushSaveLink = false
     
-        public init() {}
-    }
+    var home: HomeFeature.State = .init()
+    var storageBox: StorageBoxFeature.State = .init()
     
+    public init() {}
+  }
+  
   public enum Action: BindableAction, Equatable {
-        case binding(BindingAction<State>)
-        case centerCircleIconTapped
-        case dimmViewTapped
-        case saveLinkButtonTapped
-        
-        case storageBox(StorageBoxFeature.Action)
-        case home(HomeFeature.Action)
-    }
+    case binding(BindingAction<State>)
+    // MARK: User Action
+    case centerCircleIconTapped
+    case dimmViewTapped
+    case saveLinkButtonTapped
     
+    case storageBox(StorageBoxFeature.Action)
+    case home(HomeFeature.Action)
+  }
+  
   public var body: some ReducerOf<Self> {
-        Scope(state: \.storageBox, action: \.storageBox) { StorageBoxFeature() }
-        Scope(state: \.home, action: \.home) { HomeFeature() }
-        
-        BindingReducer()
-        
-        Reduce { state, action in
-            switch action {
-            case .centerCircleIconTapped:
-                state.showMenu.toggle()
-                return .none
-            case .dimmViewTapped:
-                state.showMenu = false
-                return .none
-            case .saveLinkButtonTapped:
-                state.pushSaveLink = true
-                return . none
-            case .storageBox:
-                return .none
-            case .home:
-                return .none
-            case .binding:
-                return .none
-            }
-        }
+    Scope(state: \.storageBox, action: \.storageBox) { StorageBoxFeature() }
+    Scope(state: \.home, action: \.home) { HomeFeature() }
+    
+    BindingReducer()
+    
+    Reduce { state, action in
+      switch action {
+      case .centerCircleIconTapped:
+        state.showMenu.toggle()
+        return .none
+      case .dimmViewTapped:
+        state.showMenu = false
+        return .none
+      case .saveLinkButtonTapped:
+        state.pushSaveLink = true
+        return . none
+      case .storageBox:
+        return .none
+      case .home:
+        return .none
+      case .binding:
+        return .none
+      }
     }
+  }
 }
