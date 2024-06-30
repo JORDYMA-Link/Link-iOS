6d4d6188b90541f376206c92f845a0a5a4d600aa
kimkyuchul <rbcjf122@gmail.com>
2024-06-30 20:06:37 +0900

    [ADD] BKTokenInterceptor (#30)

---
+++ Projects/Core/Services/Network/Foundation/BKTokenInterceptor.swift
@@ -0,0 +1,19 @@
+//
+//  BKTokenInterceptor.swift
+//  Services
+//
+//  Created by kyuchul on 6/30/24.
+//  Copyright © 2024 com.kyuchul.blink. All rights reserved.
+//
+
+import Foundation
+
+import Moya
+
+protocol Interceptorable {
+  
+}
+
+public struct BKTokenInterceptor: RequestInterceptor, Interceptorable {
+  
+}
