6a5e71af14dcb5e44a00bc2ef6f66f6fd6a0df99
kimkyuchul <rbcjf122@gmail.com>
2024-06-30 20:06:07 +0900

    [ADD] CommonMoyaProvider (#30)

---
+++ Projects/Core/Services/Network/Foundation/CommonMoyaProvider.swift
@@ -0,0 +1,29 @@
+//
+//  CommonMoyaProvider.swift
+//  Services
+//
+//  Created by kyuchul on 6/30/24.
+//  Copyright © 2024 com.kyuchul.blink. All rights reserved.
+//
+
+import Foundation
+
+import Moya
+
+final class CommonMoyaProvider<T: TargetType>: MoyaProvider<T> {
+  
+  private var isInterceptor: Bool
+  
+  init(isInterceptor: Bool = true) {
+    self.isInterceptor = isInterceptor
+    
+    let configuration = URLSessionConfiguration.af.default
+    configuration.timeoutIntervalForRequest = 10
+    configuration.timeoutIntervalForResource = 10
+    
+    let session = isInterceptor ? Session(configuration: configuration, interceptor: BKTokenInterceptor()) : Session(configuration: configuration)
+    let plugIn = NetworkLoggerPlugin()
+    
+    super.init(session: session, plugins: [plugIn])
+  }
+}
