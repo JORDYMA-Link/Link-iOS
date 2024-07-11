e91c5cb9d124408b709944942c38f046f197f581
kimkyuchul <rbcjf122@gmail.com>
2024-07-11 16:34:51 +0900

    [ADD] 폴더 아이템 디자인 시스템 추가 (#42)

--- Projects/Features/Scene/Home/EditFolderBottomSheet/EditFolderBottomSheet.swift
+++ Projects/Features/Scene/Home/EditFolderBottomSheet/EditFolderBottomSheet.swift
@@ -21,19 +21,16 @@ struct EditFolderBottomSheet: View {
       ScrollView(.horizontal, showsIndicators: false) {
         LazyHStack(spacing: 10) {
           ForEach(Array(store.folderList.enumerated()), id: \.offset) { index, folder in
-            makeFolderCell(
+            BKFolderItem(
               title: folder.title,
-              isSeleted: store.seletedFolder == folder
+              isSeleted: store.seletedFolder == folder,
+              action: { store.send(.folderCellTapped(folder), animation: .default) }
             )
-            .onTapGesture {
-              store.send(.folderCellTapped(folder), animation: .default)
-            }
           }
           
-          makeAddFolderCell()
-            .onTapGesture {
-              store.send(.addFolderBottomSheet(.addFolderTapped), animation: .default)
-            }
+          BKAddFolderItem(
+            action: { store.send(.addFolderBottomSheet(.addFolderTapped), animation: .default) }
+          )
         }
         .padding(.vertical, 12)
         .padding(.horizontal, 16)
@@ -55,40 +52,10 @@ struct EditFolderBottomSheet: View {
   }
   
   @ViewBuilder
-  private func makeFolderCell(title: String, isSeleted: Bool) -> some View {
-    let titleFontHeight = UIFont.semiBold(size: ._16).lineHeight
-    
-    Text(title)
-      .font(.semiBold(size: ._16))
-      .foregroundColor(.bkColor(isSeleted ? .white : .gray600))
-      .padding(.vertical, ((24 - titleFontHeight) / 2) + 13)
-      .padding(.horizontal, 15)
-      .background(
-        RoundedRectangle(cornerRadius: 10)
-          .fill(Color.bkColor(isSeleted ? .main300 : .gray300))
-          .stroke(Color.bkColor(isSeleted ? .main300 : .gray500), lineWidth: 1)
-          .padding(1)
-      )
-  }
-  
-  @ViewBuilder
-  private func makeAddFolderCell() -> some View {
-    BKIcon(image: CommonFeature.Images.icoPlus, color: .bkColor(.gray600), size: CGSize(width: 20, height: 20))
-      .font(.semiBold(size: ._16))
-      .padding(.vertical, 13)
-      .padding(.horizontal, 15)
-      .background(
-        RoundedRectangle(cornerRadius: 10)
-          .fill(Color.bkColor(.gray300))
-          .stroke(Color.bkColor(.gray500), lineWidth: 1)
-          .padding(1)
-      )
-  }
-  
-  @ViewBuilder
   private func makeAddFolderBottomSheet() -> some View {
     VStack(spacing: 0) {
       makeBKNavigationView(leadingType: .pop("폴더 추가"), trailingType: .pop(action: {
+        hideKeyboard()
         store.send(.addFolderBottomSheet(.closeButtonTapped))}))
       
       AddFolderBottomSheet(store: store.scope(state: \.addFolderBottomSheet, action: \.addFolderBottomSheet))
