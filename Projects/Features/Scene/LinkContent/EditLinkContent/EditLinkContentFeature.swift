bddcb7f067782a418cfe896f8c138ad0e352271a
kimkyuchul <rbcjf122@gmail.com>
2024-07-21 19:01:02 +0900

    [CHORE] 이미지 업로드 로직 구현 (#42) - 용량, 타입 체크

--- Projects/Features/Scene/LinkContent/EditLinkContent/EditLinkContentFeature.swift
+++ Projects/Features/Scene/LinkContent/EditLinkContent/EditLinkContentFeature.swift
@@ -6,10 +6,11 @@
 //  Copyright © 2024 com.kyuchul.blink. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 import Models
 import Services
+import CommonFeature
 
 import ComposableArchitecture
 
@@ -18,6 +19,14 @@ public struct EditLinkContentFeature {
   @ObservableState
   public struct State: Equatable {
     var link: LinkCard
+    var isTitleVaild: Bool = false
+    var isContentVaild: Bool = false
+    
+    var currentImage: UIImage = CommonFeature.Images.contentDetailBackgroundUIImage
+    var selectedImage: [UIImage] = []
+    var isPhotoError: PhotoPickerError?
+    
+    var isPresentedModal: Bool = false
     
     public init(link: LinkCard) {
       self.link = link
@@ -29,6 +38,8 @@ public struct EditLinkContentFeature {
     
     // MARK: User Action
     case closeButtonTapped
+    case selectedImagesChanged([UIImage])
+    case editConfirmButtonTapped
     
     // MARK: Inner Business Action
     
@@ -44,12 +55,23 @@ public struct EditLinkContentFeature {
     
     Reduce { state, action in
       switch action {
-      case .binding:
+      case .binding(\.link):
+        return .none
+        
+      case .binding(\.isPhotoError):
+        state.isPresentedModal = true
         return .none
         
       case .closeButtonTapped:
          return .run { _ in await self.dismiss() }
-                
+        
+      case let .selectedImagesChanged(images):
+        state.selectedImage = images
+        return .none
+      
+      case .editConfirmButtonTapped:
+        return .send(.closeButtonTapped)
+                        
       default:
         return .none
       }
