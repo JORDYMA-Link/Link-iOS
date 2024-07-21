9349a73f885f8b63008212df963e68243627aeab
kimkyuchul <rbcjf122@gmail.com>
2024-07-21 19:01:02 +0900

    [CHORE] 이미지 업로드 로직 구현 (#42) - 용량, 타입 체크

--- Projects/Features/Scene/LinkContent/EditLinkContent/EditLinkContentView.swift
+++ Projects/Features/Scene/LinkContent/EditLinkContent/EditLinkContentView.swift
@@ -17,24 +17,157 @@ struct EditLinkContentView: View {
   @Bindable var store: StoreOf<EditLinkContentFeature>
   
   var body: some View {
-    VStack(alignment: .leading, spacing: 0) {
+    VStack(spacing: 0) {
       makeBKNavigationView(
         leadingType: .pop("내용수정"),
         trailingType: .pop(action: { store.send(.closeButtonTapped) })
       )
       
-      BKText(
-        text: "요약 내용",
-        font: .semiBold,
-        size: ._18,
-        lineHeight: 26,
-        color: .bkColor(.gray900)
-      )
-      .padding(.top, 8)
+      VStack(alignment: .leading, spacing: 0) {
+        BKText(
+          text: "제목",
+          font: .semiBold,
+          size: ._18,
+          lineHeight: 26,
+          color: .bkColor(.gray900)
+        )
+        .padding(.top, 8)
+        
+        BKTextField(
+          text: $store.link.title,
+          isHighlight: $store.isTitleVaild,
+          textFieldType: .editLinkTitle,
+          textCount: 50,
+          isMultiLine: true,
+          height: 67
+        )
+        .padding(.top, 4)
+        
+        BKText(
+          text: "요약 내용",
+          font: .semiBold,
+          size: ._18,
+          lineHeight: 26,
+          color: .bkColor(.gray900)
+        )
+        .padding(.top, 12)
+        
+        BKTextField(
+          text: $store.link.description,
+          isHighlight: $store.isContentVaild,
+          textFieldType: .editLinkContent,
+          textCount: 200,
+          isMultiLine: true,
+          height: 160
+        )
+        .padding(.top, 4)
+        
+        HStack {
+          BKText(
+            text: "키워드",
+            font: .semiBold,
+            size: ._18,
+            lineHeight: 26,
+            color: .bkColor(.gray900)
+          )
+          
+          Spacer(minLength: 0)
+          
+          BKText(
+            text: "키워드는 최대 3개까지 지정할 수 있어요",
+            font: .regular,
+            size: ._11,
+            lineHeight: 26,
+            color: .bkColor(.gray800)
+          )
+        }
+        .padding(.top, 12)
+        
+        BKChipView(
+          keyword: LinkCard.mock().first!.keyword,
+          textColor: .bkColor(.gray700),
+          strokeColor: .bkColor(.gray500),
+          font: .semiBold(size: ._11)
+        )
+        .frame(height: 26)
+        .padding(.top, 12)
+        
+        HStack {
+          BKText(
+            text: "이미지",
+            font: .semiBold,
+            size: ._18,
+            lineHeight: 26,
+            color: .bkColor(.gray900)
+          )
+          
+          Spacer(minLength: 0)
+          
+          BKText(
+            text: "5MB 이하의 이미지만 첨부 가능해요",
+            font: .regular,
+            size: ._11,
+            lineHeight: 26,
+            color: .bkColor(.gray800)
+          )
+        }
+        .padding(.top, 12)
       
-      Spacer()
-      Spacer()
+        BKPhotoPicker(
+          selectedImages: $store.selectedImage.sending(\.selectedImagesChanged),
+          isPhotoError: $store.isPhotoError
+        ) {
+          makePhoto(
+            currentImage: store.currentImage,
+            selectedImages: store.selectedImage
+          )
+        }
+        .padding(.top, 12)
+      }
+      .padding(.horizontal, 16)
+      
+      BKRoundedButton(
+        title: "수정 완료",
+        isDisabled: store.isTitleVaild && store.isContentVaild,
+        confirmAction: { store.send(.editConfirmButtonTapped) }
+      )
+      .padding(.horizontal, 16)
+      .padding(.bottom, 14)
     }
-    .padding(.horizontal, 16)
+    .modal(
+      isPresented: $store.isPresentedModal,
+      type: store.isPhotoError == .type ? .photoTypeError(checkAction: {}, cancelAction: { store.isPresentedModal = false }) : .photoSizeError(checkAction: {}, cancelAction: { store.isPresentedModal = false })
+    )
+  }
+  
+  @MainActor
+  @ViewBuilder
+  private func makePhoto(currentImage: UIImage, selectedImages: [UIImage]) -> some View {
+    if !selectedImages.isEmpty {
+      makeImageView(Image(uiImage: selectedImages[0]))
+    } else {
+      makeImageView(Image(uiImage: currentImage))
+    }
+  }
+  
+  private func makeImageView(_ image: Image) -> some View {
+    image
+      .resizable()
+      .scaledToFill()
+      .frame(width: 80, height: 80, alignment: .leading)
+      .clipShape(RoundedRectangle(cornerRadius: 10))
+      .overlay(alignment: .center) {
+        VStack(spacing: 4) {
+          BKIcon(
+            image: CommonFeature.Images.icoImg,
+            color: .white,
+            size: .init(width: 24, height: 24)
+          )
+          
+          Text("이미지 수정하기")
+            .foregroundStyle(Color.white)
+            .font(.semiBold(size: ._11))
+        }
+      }
   }
 }
