23f49043f20e39af0df0f7412a43f21f0edfb9c8
kimkyuchul <rbcjf122@gmail.com>
2024-07-22 15:01:55 +0900

    [CHORE] 메모 바텀 시트 디자인 수정 반영 (#42)

--- Projects/Features/Scene/LinkContent/LinkContent/LinkContentView.swift
+++ Projects/Features/Scene/LinkContent/LinkContent/LinkContentView.swift
@@ -143,7 +143,7 @@ struct LinkContentView: View {
     }
     .bottomSheet(
       isPresented: $store.editMemoBottomSheet.isEditMemoBottomSheetPresented,
-      detents: [.height(256)],
+      detents: [.large],
       leadingTitle: "메모",
       closeButtonAction: { store.send(.editMemoBottomSheet(.closeButtonTapped)) }
     ) {
