cf8704c11a433c4ffd831c4be6894c66abcb5fbf
kimkyuchul <rbcjf122@gmail.com>
2024-07-29 10:32:23 +0900

    [ADD] StorageBoxContainerView 구현 (#48)

---
+++ Projects/Features/Scene/StorageBox/StorageBoxContainerView.swift
@@ -0,0 +1,40 @@
+//
+//  StorageBoxContainerView.swift
+//  Features
+//
+//  Created by kyuchul on 7/29/24.
+//  Copyright © 2024 com.kyuchul.blink. All rights reserved.
+//
+
+import SwiftUI
+
+import CommonFeature
+
+import ComposableArchitecture
+
+struct StorageBoxContainerView<Content: View>: View {
+  private let store: StoreOf<StorageBoxFeature>
+  private let tabbar: () -> Content
+  
+  public init(
+    store: StoreOf<StorageBoxFeature>,
+    tabbar: @autoclosure @escaping () -> Content
+  ) {
+    self.store = store
+    self.tabbar = tabbar
+  }
+  
+  var body: some View {
+    NavigationStack {
+      ZStack(alignment: .bottom) {
+        StorageBoxView(store: store)
+        tabbar()
+      }
+      .addFolderBottomSheet(store: store)
+      .settingStorageBoxBottomSheet(store: store)
+      .editFolderNameBottomSheet(store: store)
+      .deleteFolderModal(store: store)
+      .toolbar(.hidden, for: .navigationBar)
+    }
+  }
+}
