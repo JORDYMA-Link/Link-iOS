03d7147ef8f26b3593c50dcf6109db6a59ad6cf6
kimkyuchul <rbcjf122@gmail.com>
2024-08-08 17:49:41 +0900

    [ADD] Folder Response (#56)

---
+++ Projects/Core/Services/Network/FolderAPI/Response/OnboardingFolderResponse.swift
@@ -0,0 +1,19 @@
+//
+//  OnboardingFolderResponse.swift
+//  Services
+//
+//  Created by kyuchul on 8/8/24.
+//  Copyright © 2024 com.kyuchul.blink. All rights reserved.
+//
+
+import Foundation
+
+struct OnboardingFolderResponse: Decodable {
+  let ids: [Int]
+}
+
+extension OnboardingFolderResponse {
+  public func toDomain() -> OnboardingFolder {
+    return OnboardingFolder(ids: ids)
+  }
+}
