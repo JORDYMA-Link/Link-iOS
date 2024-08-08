87adb320350d5cf3a37cc75874317bb454935bfd
kimkyuchul <rbcjf122@gmail.com>
2024-08-08 17:47:14 +0900

    [CHORE] Folder Entity 수정 (#56)

---
+++ Projects/Core/Models/Folder/Folder.swift
@@ -0,0 +1,35 @@
+//
+//  Folder.swift
+//  CoreKit
+//
+//  Created by kyuchul on 6/20/24.
+//  Copyright © 2024 com.kyuchul.blink. All rights reserved.
+//
+
+import Foundation
+
+public struct Folder: Identifiable, Equatable {
+  public var id: Int
+  public var name: String
+  public var feedCount: Int
+  
+  public init(
+    id: Int,
+    name: String,
+    feedCount: Int
+  ) {
+    self.id = id
+    self.name = name
+    self.feedCount = feedCount
+  }
+}
+
+extension Folder {
+  public static func makeFolderMock() -> [Folder] {
+    return [
+      .init(id: 23, name: "아르지닌6000", feedCount: 990),
+      .init(id: 24, name: "아르지닌6000", feedCount: 990),
+      .init(id: 25, name: "아르지닌6000", feedCount: 990)
+    ]
+  }
+}
