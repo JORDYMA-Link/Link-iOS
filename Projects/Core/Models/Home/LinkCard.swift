70d695b35c6a84eba4d4f99dd2ceb3c76ef82474
kimkyuchul <rbcjf122@gmail.com>
2024-08-08 17:47:53 +0900

    [CHORE] UUID String 제거 (뷰 다시 그려지는 이슈) (#56)

--- Projects/Core/Models/Home/LinkCard.swift
+++ Projects/Core/Models/Home/LinkCard.swift
@@ -9,14 +9,15 @@
 import Foundation
 
 public struct LinkCard: Hashable, Identifiable {
-  public var id = UUID().uuidString
+  public var id: Int
   public var sourceTitle: String
   public var sourceImage: String
   public var title: String
   public var description: String
   public var keyword: [String]
   
-  public init(sourceTitle: String, sourceImage: String, title: String, description: String, keyword: [String]) {
+  public init(id: Int, sourceTitle: String, sourceImage: String, title: String, description: String, keyword: [String]) {
+    self.id = id
     self.sourceTitle = sourceTitle
     self.sourceImage = sourceImage
     self.title = title
@@ -28,18 +29,18 @@ public struct LinkCard: Hashable, Identifiable {
 extension LinkCard {
   public static func mock() -> [LinkCard] {
     return [
-      .init(sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
-      .init(sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
-      .init(sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
-      .init(sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
-      .init(sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
-      .init(sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
-      .init(sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
-      .init(sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
-      .init(sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
-      .init(sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
-      .init(sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
-      .init(sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"])
+      .init(id: 1, sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
+      .init(id: 2, sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
+      .init(id: 3, sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
+      .init(id: 4, sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
+      .init(id: 5, sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
+      .init(id: 6, sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
+      .init(id: 7, sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
+      .init(id: 8, sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
+      .init(id: 9, sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
+      .init(id: 10, sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
+      .init(id: 11, sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
+      .init(id: 12, sourceTitle: "미디엄", sourceImage: "Image", title: "방문자 상위 50위 생성형 AI 웹 서비스 분석", description: "꽁꽁얼어붙은", keyword: ["Design System", "디자인", "UI/UX"]),
     ]
   }
 }
