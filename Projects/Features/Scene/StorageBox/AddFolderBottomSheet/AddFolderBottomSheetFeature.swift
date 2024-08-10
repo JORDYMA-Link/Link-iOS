aa42941c0d1838fdf3d9d5c70c648d0de15cc859
kimkyuchul <rbcjf122@gmail.com>
2024-08-10 22:48:55 +0900

    [ADD] 폴더 추가 API 연결 (#58)

--- Projects/Features/Scene/StorageBox/AddFolderBottomSheet/AddFolderBottomSheetFeature.swift
+++ Projects/Features/Scene/StorageBox/AddFolderBottomSheet/AddFolderBottomSheetFeature.swift
@@ -9,17 +9,17 @@
 import Foundation
 
 import Models
+import Services
 
 import ComposableArchitecture
 
 @Reducer
-public struct AddFolderBottomSheetFeature: Reducer {
+public struct AddFolderBottomSheetFeature {
   @ObservableState
   public struct State: Equatable {
+    public var folderName: String = ""
+    public var isValidation: Bool = true
     public var isAddFolderBottomSheetPresented: Bool = false
-    public var isHighlight: Bool = true
-    public var folderInput: Folder = .init(id: 0, name: "", feedCount: 0)
-    public var savedFolder: Folder?
     
     public init() {}
   }
@@ -28,14 +28,23 @@ public struct AddFolderBottomSheetFeature: Reducer {
     case binding(BindingAction<State>)
     // MARK: User Action
     case addFolderTapped
-    case confirmButtonTapped
     case closeButtonTapped
+    case confirmButtonTapped
     
+    // MARK: Inner Business Action
+    case successAddFolder
+        
     // MARK: Delegate Action
     public enum Delegate {
-      case didUpdateFolderList
+      case fetchFolderList
     }
     case delegate(Delegate)
+  }
+  
+  @Dependency(\.folderClient) private var folderClient
+  
+  private enum ThrottleId {
+    case confirmButton
   }
   
   public var body: some ReducerOf<Self> {
@@ -43,23 +52,37 @@ public struct AddFolderBottomSheetFeature: Reducer {
     
     Reduce { state, action in
       switch action {
-      case .binding(\.folderInput):
+      case .binding(\.folderName):
         return .none
         
       case .addFolderTapped:
         state.isAddFolderBottomSheetPresented = true
         return .none
         
-      case .confirmButtonTapped:
-        state.savedFolder = state.folderInput
-        state.isAddFolderBottomSheetPresented = false
-        return .send(.delegate(.didUpdateFolderList))
-        
       case .closeButtonTapped:
-        state.folderInput = .init(id: 0, name: "", feedCount: 0)
+        state.folderName = ""
         state.isAddFolderBottomSheetPresented = false
         return .none
-                
+        
+      case .confirmButtonTapped:
+        return .run(
+          operation: { [state] send in
+            let addFolder = try await folderClient.postFolder(state.folderName)
+            
+            print(addFolder)
+            
+            await send(.successAddFolder)
+          },
+          catch: { error, send in
+            print(error)
+          }
+        )
+        .throttle(id: ThrottleId.confirmButton, for: .seconds(1), scheduler: DispatchQueue.main, latest: false)
+        
+      case .successAddFolder:
+        state.isAddFolderBottomSheetPresented = false
+        return .send(.delegate(.fetchFolderList))
+                        
       default:
         return .none
       }
