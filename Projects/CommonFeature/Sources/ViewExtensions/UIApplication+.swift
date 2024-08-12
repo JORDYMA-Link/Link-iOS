f844cc328168576e0d6ae6582cac506ea31549c9
kimkyuchul <rbcjf122@gmail.com>
2024-08-12 18:00:32 +0900

    [ADD] UIApplication window extension 추가 (#58)

--- Projects/CommonFeature/Sources/ViewExtensions/UIApplication+.swift
+++ Projects/CommonFeature/Sources/ViewExtensions/UIApplication+.swift
@@ -9,19 +9,32 @@
 import SwiftUI
 
 public extension UIApplication {
-    static var topSafeAreaInset: CGFloat {
-      let safeAreaInset = UIApplication.shared.connectedScenes.first
-          .flatMap { ($0 as? UIWindowScene)?.windows.first }?
-          .flatMap { $0.safeAreaInsets }
-        
-      return safeAreaInset?.top ?? .zero
-    }
+  static var topSafeAreaInset: CGFloat {
+    let safeAreaInset = UIApplication.shared.connectedScenes.first
+      .flatMap { ($0 as? UIWindowScene)?.windows.first }?
+      .flatMap { $0.safeAreaInsets }
     
-    static var bottomSafeAreaInset: CGFloat {
-      let safeAreaInset = UIApplication.shared.connectedScenes.first
-          .flatMap { ($0 as? UIWindowScene)?.windows.first }?
-          .flatMap { $0.safeAreaInsets }
-        
-        return safeAreaInset?.bottom ?? .zero
-    }
+    return safeAreaInset?.top ?? .zero
+  }
+  
+  static var bottomSafeAreaInset: CGFloat {
+    let safeAreaInset = UIApplication.shared.connectedScenes.first
+      .flatMap { ($0 as? UIWindowScene)?.windows.first }?
+      .flatMap { $0.safeAreaInsets }
+    
+    return safeAreaInset?.bottom ?? .zero
+  }
+  
+  var firstKeyWindow: UIWindow? {
+    return UIApplication.shared.connectedScenes
+      .compactMap { $0 as? UIWindowScene }
+      .filter { $0.activationState == .foregroundActive }
+      .first?.keyWindow
+  }
+  
+  var firstWindowScene: UIWindowScene? {
+    return UIApplication.shared.connectedScenes
+      .compactMap { $0 as? UIWindowScene }
+      .first { $0.windows.contains(where: \.isKeyWindow) }
+  }
 }
