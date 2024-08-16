4077f96e47d350364e14ef47574131e5a6c560e1
kimkyuchul <rbcjf122@gmail.com>
2024-08-16 16:19:01 +0900

    [CHORE] BKRoundedButton 분기처리 (#60)

--- Projects/CommonFeature/Sources/DesignSystem/Component/Button/BKRoundedButton.swift
+++ Projects/CommonFeature/Sources/DesignSystem/Component/Button/BKRoundedButton.swift
@@ -8,18 +8,53 @@
 
 import SwiftUI
 
+public enum BKRoundedButtonType {
+  case main
+  case gray
+  
+  var tintColor: BKColor {
+    switch self {
+    case .main:
+      return .white
+    case .gray:
+      return .gray900
+    }
+  }
+  
+  var backgroundColor: BKColor {
+    switch self {
+    case .main:
+      return .main300
+    case .gray:
+      return .gray300
+    }
+  }
+  
+  var stroke: CGFloat {
+    switch self {
+    case .main:
+      return 0
+    case .gray:
+      return 1
+    }
+  }
+}
+
 public struct BKRoundedButton: View {
-  private var title: String
-  private var isDisabled: Bool
-  private var confirmAction: () -> Void
+  private let buttonType: BKRoundedButtonType
+  private let title: String
+  private let isDisabled: Bool
+  private let confirmAction: () -> Void
   private var isCornerRadius = false
   
   public init(
+    buttonType: BKRoundedButtonType = .main,
     title: String,
     isDisabled: Bool = false,
     isCornerRadius: Bool = true,
     confirmAction: @escaping () -> Void
   ) {
+    self.buttonType = buttonType
     self.title = title
     self.isDisabled = isDisabled
     self.confirmAction = confirmAction
@@ -32,12 +67,16 @@ public struct BKRoundedButton: View {
     } label: {
       ZStack {
         RoundedRectangle(cornerRadius: isCornerRadius ? 10 : 0, style: .continuous)
-          .fill(Color.bkColor(isDisabled ? .gray500 : .main300))
+          .fill(Color.bkColor(isDisabled ? .gray500 : buttonType.backgroundColor))
           .frame(maxWidth: .infinity)
+          .overlay {
+            RoundedRectangle(cornerRadius: isCornerRadius ? 10 : 0, style: .continuous)
+              .stroke(Color.bkColor(.gray500), lineWidth: buttonType.stroke)
+          }
         
         Text(title)
           .font(.semiBold(size: ._16))
-          .foregroundStyle(Color.bkColor(isDisabled ? .gray600 : .white))
+          .foregroundStyle(Color.bkColor(isDisabled ? .gray600 : buttonType.tintColor))
           .padding(EdgeInsets(top: 14, leading: 16, bottom: 14, trailing: 16))
       }
       .frame(height: 52)
