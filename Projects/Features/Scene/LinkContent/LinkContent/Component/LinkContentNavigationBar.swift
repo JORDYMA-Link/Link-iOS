700373308cf2bee476688fa17b7f4838cb9b4e01
kimkyuchul <rbcjf122@gmail.com>
2024-08-16 10:44:07 +0900

    [CHORE] 스크롤 state 인스턴스 이름 수정 (#60)

--- Projects/Features/Scene/LinkContent/LinkContent/Component/LinkContentNavigationBar.swift
+++ Projects/Features/Scene/LinkContent/LinkContent/Component/LinkContentNavigationBar.swift
@@ -12,19 +12,18 @@ import CommonFeature
 import Common
 
 struct LinkContentNavigationBar: View {
-  
-  @Binding var isScrolled: Bool
+  @Binding var isScrollDetected: Bool
   private let title: String
   private let leftAction: () -> Void
   private let rightAction: () -> Void
   
   init(
-    isScrolled: Binding<Bool>,
+    isScrollDetected: Binding<Bool>,
     title: String,
     leftAction: @escaping () -> Void,
     rightAction: @escaping () -> Void
   ) {
-    self._isScrolled = isScrolled
+    self._isScrollDetected = isScrollDetected
     self.title = title
     self.leftAction = leftAction
     self.rightAction = rightAction
@@ -40,7 +39,7 @@ struct LinkContentNavigationBar: View {
         } label: {
           BKIcon(
             image: CommonFeature.Images.icoChevronLeft,
-            color: isScrolled ? .black : .white,
+            color: isScrollDetected ? .black : .white,
             size: CGSize(width: 24, height: 24)
           )
         }
@@ -51,7 +50,7 @@ struct LinkContentNavigationBar: View {
           .lineLimit(1)
           .frame(maxWidth: .infinity, alignment: .leading)
           .padding(.leading, 4)
-          .opacity(isScrolled ? 1 : 0)
+          .opacity(isScrollDetected ? 1 : 0)
         
         Spacer(minLength: 0)
         
@@ -60,7 +59,7 @@ struct LinkContentNavigationBar: View {
         } label: {
           BKIcon(
             image: CommonFeature.Images.icoMoreVertical,
-            color: isScrolled ? .black : .white,
+            color: isScrollDetected ? .black : .white,
             size: CGSize(width: 24, height: 24)
           )
         }
@@ -72,10 +71,10 @@ struct LinkContentNavigationBar: View {
       Divider()
         .foregroundStyle(Color.bkColor(.gray400))
         .frame(maxWidth: .infinity)
-        .opacity(isScrolled ? 1 : 0)
+        .opacity(isScrollDetected ? 1 : 0)
     }
     .frame(height: 56)
     .padding(.top, UIApplication.topSafeAreaInset)
-    .background(isScrolled ? Color.white : Color.clear)
+    .background(isScrollDetected ? Color.white : Color.clear)
   }
 }
