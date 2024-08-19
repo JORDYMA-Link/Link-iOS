118742406873a56312c1c06e55b9f03318e8930e
kimkyuchul <rbcjf122@gmail.com>
2024-08-19 14:38:35 +0900

    [CHORE] BKChipView 분기처리 추가 (#60)

--- Projects/CommonFeature/Sources/DesignSystem/BKChipView.swift
+++ Projects/CommonFeature/Sources/DesignSystem/BKChipView.swift
@@ -9,31 +9,53 @@
 import SwiftUI
 
 public enum ChipType {
+  /// 추가, 삭제 불가
   case `default`
-  case edit
+  /// 삭제만 가능
+  case delete
+  /// 추가, 삭제 가능
+  case addWithDelete
 }
 
 public struct BKChipView: View {
   @Binding var keywords: [String]
   private let chipType: ChipType
+  private let deleteAction: ((String) -> Void)?
+  private let addAction: (() -> Void)?
   
   public init(
     keywords: Binding<[String]>,
-    chipType: ChipType
+    chipType: ChipType,
+    deleteAction: ((String) -> Void)? = nil,
+    addAction: (() -> Void)? = nil
   ) {
     self._keywords = keywords
     self.chipType = chipType
+    self.deleteAction = deleteAction
+    self.addAction = addAction
   }
   
   public var body: some View {
     ScrollView(.horizontal, showsIndicators: false) {
       LazyHStack(spacing: 4) {
         ForEach(keywords, id: \.self) { keyword in
-          ChipItem(title: keyword, type: chipType)
+          ChipItem(
+            title: keyword,
+            type: chipType,
+            isAdd: false,
+            deleteAction: deleteAction,
+            addAction: addAction
+          )
         }
         
-        if chipType == .edit {
-          ChipItem(title: "추가하기", type: chipType, isAdd: true)
+        if chipType == .addWithDelete {
+          ChipItem(
+            title: "추가하기",
+            type: chipType,
+            isAdd: true,
+            deleteAction: deleteAction,
+            addAction: addAction
+          )
         }
       }
     }
@@ -45,15 +67,21 @@ private struct ChipItem: View {
   private let title: String
   private let type: ChipType
   private var isAdd: Bool
+  private let deleteAction: ((String) -> Void)?
+  private let addAction: (() -> Void)?
   
   init(
     title: String,
     type: ChipType,
-    isAdd: Bool = false
+    isAdd: Bool,
+    deleteAction: ((String) -> Void)?,
+    addAction: (() -> Void)?
   ) {
     self.title = title
     self.type = type
     self.isAdd = isAdd
+    self.deleteAction = deleteAction
+    self.addAction = addAction
   }
   
   var body: some View {
@@ -67,12 +95,15 @@ private struct ChipItem: View {
       )
       .lineLimit(1)
       
-      if type == .edit {
+      if type != .default {
         BKIcon(
           image: isAdd ? CommonFeature.Images.icoPlus : CommonFeature.Images.icoClose,
           color: .bkColor(.gray700),
           size: .init(width: 12, height: 12)
         )
+        .onTapGesture {
+          isAdd ? addAction?() : deleteAction?(title)
+        }
       }
     }
     .padding(.vertical, 4)
