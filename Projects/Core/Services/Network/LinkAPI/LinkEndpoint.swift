5aecfa9f716328df5b0311f48b57a4c1c904ab8c
kimkyuchul <rbcjf122@gmail.com>
2024-08-20 10:53:31 +0900

    [ADD] LinkEndpoint (#64) - 멀티파트폼 제외

---
+++ Projects/Core/Services/Network/LinkAPI/LinkEndpoint.swift
@@ -0,0 +1,71 @@
+//
+//  LinkEndpoint.swift
+//  Models
+//
+//  Created by kyuchul on 8/19/24.
+//  Copyright © 2024 com.kyuchul.blink. All rights reserved.
+//
+
+import Foundation
+
+import Moya
+
+enum LinkEndpoint {
+  case postLinkSummary(link: String, content: String)
+  case postLinkImage(feedId: String)
+  case patchLink(feedId: String, folderName: String, title: String, summary: String, keywords: [String], memo: String)
+  case getLinkProcessing
+  case deleteLinkDenySummary(feedId: String)
+}
+
+extension LinkEndpoint: BaseTargetType {
+  var path: String {
+    let baseLinkRoutePath: String = "/api/feeds"
+    
+    switch self {
+    case .postLinkSummary:
+      return baseLinkRoutePath + "/summary"
+    case let .postLinkImage(feedId):
+      return baseLinkRoutePath + "/image/\(feedId)"
+    case let .patchLink(feedId, _, _, _, _, _):
+      return baseLinkRoutePath + "/\(feedId)"
+    case .getLinkProcessing:
+      return baseLinkRoutePath + "/processing"
+    case let .deleteLinkDenySummary(feedId):
+      return baseLinkRoutePath + "/processing/\(feedId)"
+    }
+  }
+  
+  var method: Moya.Method {
+    switch self {
+    case .postLinkSummary, .postLinkImage:
+      return .post
+    case .patchLink:
+      return .patch
+    case .getLinkProcessing:
+      return .get
+    case .deleteLinkDenySummary:
+      return .delete
+    }
+  }
+  
+  var task: Moya.Task {
+    switch self {
+    case let .postLinkSummary(link, content):
+      return .requestParameters(parameters: [
+        "link": link,
+        "content": content
+      ], encoding: JSONEncoding.default)
+      
+    case let .postLinkImage(feedId):
+      return .uploadMultipart([])
+      
+    case let .patchLink(_, folderName, title, summary, keywords, memo):
+      let patchLinkBody = PatchLinkRequest(folderName: folderName, title: title, summary: summary, keywords: keywords, memo: memo)
+      return .requestJSONEncodable(patchLinkBody)
+      
+    case .getLinkProcessing, .deleteLinkDenySummary:
+      return .requestPlain
+    }
+  }
+}
