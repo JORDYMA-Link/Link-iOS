2a8440716877041b5a5d35cf30a1504cab14f6eb
kimkyuchul <rbcjf122@gmail.com>
2024-08-28 15:46:37 +0900

    [ADD] 피드 삭제 API 구현 (#64)

--- Projects/Core/Services/Network/FeedAPI/FeedClient.swift
+++ Projects/Core/Services/Network/FeedAPI/FeedClient.swift
@@ -16,6 +16,8 @@ import Moya
 public struct FeedClient {
   /// 피드 상세 메모 수정
   public var postFeedMemo: @Sendable (_ feedId: Int, _ memo: String) async throws -> Feed
+  /// 피드 삭제
+  public var deleteFeed: @Sendable (_ feedId: Int) async throws -> Void
   /// 피드 북마크 여부 변경
   public var patchBookmark: @Sendable (_ feedId: Int, _ setMarked: Bool) async throws -> FeedBookmark
   /// 피드 상세 조회
@@ -31,6 +33,9 @@ extension FeedClient: DependencyKey {
         let responseDTO: FeedResponse = try await feedProvider.request(.postFeedMemo(feedId: feedId, memo: memo), modelType: FeedResponse.self)
         
         return responseDTO.toDomain()
+      },
+      deleteFeed: { feedId in
+        return try await feedProvider.requestPlain(.deleteFeed(feedId: feedId))
       },
       patchBookmark: { feedId, setMarked in
         let responseDTO: FeedBookmarkResponse = try await feedProvider.request(.patchBookmark(feedId: feedId, setMarked: setMarked), modelType: FeedBookmarkResponse.self)
