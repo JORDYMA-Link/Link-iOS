240ea33dfd20be4e7c905f0d1d2b79eb76a00fab
J.H. Moon <moonjh587@naver.com>
2024-08-28 21:57:39 +0900

    [CHORE] - #62 주석 삭제

--- Projects/Core/Services/Network/SettingAPI/SettingClient.swift
+++ Projects/Core/Services/Network/SettingAPI/SettingClient.swift
@@ -16,11 +16,6 @@ import Moya
 public struct SettingClient {
   public var getUserProfile: @Sendable () async throws -> Setting
   public var requestUserProfile: @Sendable (_ nickname: String) async throws -> Setting
-//  public var getFolders: @Sendable () async throws -> [Folder]
-//  public var postFolder: @Sendable (_ name: String) async throws -> Folder
-//  public var postOnboardingFolder: @Sendable (_ topics: [String]) async throws -> OnboardingFolder
-//  public var deleteFolder: @Sendable (_ folderId: Int) async throws -> Void
-//  public var fetchFolder: @Sendable (_ folderId: Int, _ name: String) async throws -> Folder
 }
 
 extension SettingClient: DependencyKey {
@@ -37,27 +32,6 @@ extension SettingClient: DependencyKey {
         return responseDTO.toDomain()
       }
     )
-//    return Self(
-//      getFolders: {
-//        let responseDTO: FolderListResponse = try await folderProvider.request(.getFolders, modelType: FolderListResponse.self)
-//        return responseDTO.toDomain()
-//      },
-//      postFolder: { name in
-//        let responseDTO: FolderResponse = try await folderProvider.request(.postFolder(name: name), modelType: FolderResponse.self)
-//        return responseDTO.toDomain()
-//      },
-//      postOnboardingFolder: { topics in
-//        let responseDTO: OnboardingFolderResponse = try await folderProvider.request(.postOnboardingFolder(topics: topics), modelType: OnboardingFolderResponse.self)
-//        return responseDTO.toDomain()
-//      },
-//      deleteFolder: { folderId in
-//        return try await folderProvider.requestPlain(.deleteFolder(folderId: folderId))
-//      },
-//      fetchFolder: { folderId, name in
-//        let responseDTO: FolderResponse = try await folderProvider.request(.fetchFolder(folderId: folderId, name: name), modelType: FolderResponse.self)
-//        return responseDTO.toDomain()
-//      }
-//    )
   }
 }
 
