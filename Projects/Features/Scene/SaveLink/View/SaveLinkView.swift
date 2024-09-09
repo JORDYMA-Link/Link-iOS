2e660ad93850bc836e2a4f759306b6fe66159d5f
kimkyuchul <rbcjf122@gmail.com>
2024-09-09 11:04:14 +0900

    [CHORE] 링크 저장 로직 수정 (#78) - 링크 저장 버튼 -> 모달 오픈 -> 요약 완료 시 모달 닫히고 링크 요약 완료 뷰로 이동

--- Projects/Features/Scene/SaveLink/View/SaveLinkView.swift
+++ Projects/Features/Scene/SaveLink/View/SaveLinkView.swift
@@ -10,102 +10,81 @@ import SwiftUI
 import CommonFeature
 
 import ComposableArchitecture
- 
-public struct SaveLinkView: View {
-  @Environment(\.dismiss) private var dismiss
-  @Perception.Bindable var store: StoreOf<SaveLinkFeature>
-    
-    public var body: some View {
-      WithPerceptionTracking {
-        VStack(alignment: .leading) {
-          HStack {
-            Text("링크")
-                .foregroundStyle(Color.bkColor(.main300))
-            Text("를 입력해주세요")
-                .foregroundStyle(Color.bkColor(.gray900))
-          }
-          .font(.semiBold(size: ._24))
-          .padding(EdgeInsets(top: 0, leading: 0, bottom: 4, trailing: 0))
-              
-              Text("블링크가 무엇이든 요약해줍니다")
-                  .frame(alignment: .leading)
-                  .font(.regular(size: ._14))
-                  .foregroundStyle(Color.bkColor(.gray700))
-                  .padding(EdgeInsets(top: 0, leading: 0, bottom: 24, trailing: 0))
-              
-              HStack(alignment: .top) {
-                   
-                if store.state.isValidationURL {
-                  ClearableTextField(
-                    text: $store.urlText,
-                    placeholder: "링크를 붙여주세요")
-                } else {
-                  VStack(alignment: .leading) {
-                    ClearableTextField(
-                      text: $store.urlText,
-                      placeholder: "링크를 붙여주세요")
-                    .background(Color.bkColor(.white))
-                    .overlay(
-                      RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.bkColor(.red), lineWidth: 1)
-                    )
-                    
-                    Text(store.validationReasonText)
-                      .font(.regular(size: ._12))
-                      .foregroundStyle(Color.bkColor(.red))
-                  }
-                }
-                
-                Button {
-                  self.store.send(.onTapNextButton)
-                  hideKeyboard()
-                } label: {
-                  CommonFeature.Images.icoChevronRight
-                        .renderingMode(.template)
-                        .foregroundStyle(store.saveButtonActive ?
-                                         Color.bkColor(.white) : Color.bkColor(.gray800))
-                }
-                .frame(width: 46, height: 46)
-                .background(store.saveButtonActive ?
-                            Color.bkColor(.main300) : Color.bkColor(.gray300))
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-              }
-          }
-          .padding(EdgeInsets(top: 28, leading: 16, bottom: 0, trailing: 16))
-          .navigationBarBackButtonHidden()
-          .toolbar {
-              ToolbarItem(placement: .topBarLeading) {
-                  Button(action: {
-                    dismiss() //FIXME: store로 넘기는 방법이 있을지 고민해봐야하는 요소
-                  }, label: {
-                      HStack{
-                        CommonFeature.Images.icoChevronLeft
-                          Text("링크 저장")
-                          .font(.semiBold(size: ._14))
-                              .foregroundStyle(Color.bkColor(.gray800))
-                      }
-                  })
-                  
-              }
-          }
-          .modal(
-            isPresented: $store.presentLoading,
-            type: .linkLoading(
-              checkAction: {
-                store.send(.onTapBackToMain)
-                dismiss()
-              }))
-        Spacer()
-      }
-    }
-}
 
-#Preview {
-    NavigationStack(root: {
-      SaveLinkView(store: Store(initialState: {
-        SaveLinkFeature.State()
-      }(), reducer: {
-        SaveLinkFeature()
-      }))
-    })
+public struct SaveLinkView: View {
+  @Perception.Bindable var store: StoreOf<SaveLinkFeature>
+  @Environment(\.dismiss) private var dismiss
+  
+  public var body: some View {
+    WithPerceptionTracking {
+      VStack(alignment: .leading) {
+        HStack {
+          Text("링크")
+            .foregroundStyle(Color.bkColor(.main300))
+          Text("를 입력해주세요")
+            .foregroundStyle(Color.bkColor(.gray900))
+        }
+        .font(.semiBold(size: ._24))
+        .padding(EdgeInsets(top: 0, leading: 0, bottom: 4, trailing: 0))
+        
+        Text("블링크가 무엇이든 요약해줍니다")
+          .frame(alignment: .leading)
+          .font(.regular(size: ._14))
+          .foregroundStyle(Color.bkColor(.gray700))
+          .padding(EdgeInsets(top: 0, leading: 0, bottom: 24, trailing: 0))
+        
+        HStack(alignment: .top) {
+          
+          VStack(alignment: .leading) {
+            ClearableTextField(
+              text: $store.urlText,
+              placeholder: "링크를 붙여주세요")
+            .background(Color.bkColor(.white))
+            .overlay(
+              RoundedRectangle(cornerRadius: 10)
+                .stroke(!store.isValidationURL ? Color.bkColor(.red) : Color.clear, lineWidth: 1)
+            )
+            
+            if !store.state.isValidationURL {
+              Text(store.validationReasonText)
+                .font(.regular(size: ._12))
+                .foregroundStyle(Color.bkColor(.red))
+            }
+          }
+          
+          Button {
+            store.send(.onTapNextButton, animation: .default)
+            hideKeyboard()
+          } label: {
+            CommonFeature.Images.icoChevronRight
+              .renderingMode(.template)
+              .foregroundStyle(store.saveButtonActive ?
+                               Color.bkColor(.white) : Color.bkColor(.gray800))
+          }
+          .frame(width: 46, height: 46)
+          .background(store.saveButtonActive ?
+                      Color.bkColor(.main300) : Color.bkColor(.gray300))
+          .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+      }
+      .padding(EdgeInsets(top: 28, leading: 16, bottom: 0, trailing: 16))
+      .navigationBarBackButtonHidden()
+      .toolbar {
+        ToolbarItem(placement: .topBarLeading) {
+          Button(action: {
+            store.send(.onTapBackButton)
+          }, label: {
+            HStack{
+              CommonFeature.Images.icoChevronLeft
+              Text("링크 저장")
+                .font(.semiBold(size: ._14))
+                .foregroundStyle(Color.bkColor(.gray800))
+            }
+          })
+          
+        }
+      }
+      Spacer()
+    }
+  }
 }
