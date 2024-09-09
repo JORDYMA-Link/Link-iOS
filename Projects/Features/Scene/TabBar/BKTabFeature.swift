09aeb076c22ba2f8553ff4f37277fa8832659e45
kimkyuchul <rbcjf122@gmail.com>
2024-09-09 21:42:11 +0900

    [ADD] TabView 링크 요약 리스트 NavigationStack 로직 구현 (#78)

--- Projects/Features/Scene/TabBar/BKTabFeature.swift
+++ Projects/Features/Scene/TabBar/BKTabFeature.swift
@@ -14,25 +14,33 @@ import ComposableArchitecture
 public struct BKTabFeature {
   public init() {}
   
+  @Reducer(state: .equatable)
+  public enum Path {
+    case SaveLink(SaveLinkFeature)
+    case SummaryStatus(SummaryStatusFeature)
+    case Link(LinkFeature)
+  }
+    
   @ObservableState
   public struct State: Equatable {
     var currentItem: BKTabViewType = .home
+    var path = StackState<Path.State>()
     var isSaveContentPresented = false
     
-    @Presents var saveLink: SaveLinkFeature.State?
     var home: HomeFeature.State = .init()
     var storageBox: StorageBoxFeature.State = .init()
     
     public init() {}
   }
-  
+    
   public enum Action: BindableAction {
     case binding(BindingAction<State>)
     // MARK: User Action
     case roundedTabIconTapped
     case saveLinkButtonTapped
+    case routeSummaryStatusButtonTapped
     
-    case saveLink(PresentationAction<SaveLinkFeature.Action>)
+    case path(StackAction<Path.State, Path.Action>)
     case storageBox(StorageBoxFeature.Action)
     case home(HomeFeature.Action)
   }
@@ -54,15 +62,38 @@ public struct BKTabFeature {
 
       case .saveLinkButtonTapped:
         state.isSaveContentPresented.toggle()
-        state.saveLink = .init()
+        state.path.append(.SaveLink(SaveLinkFeature.State()))
+        return .none
+        
+      case .routeSummaryStatusButtonTapped:
+        state.path.append(.SummaryStatus(SummaryStatusFeature.State()))
+        return .none
+        
+      // 링크요약 리스트 화면 Delegate
+      case let .path(.element(id: _, action: .SummaryStatus(.delegate(.summaryStatusItemTapped(feedId))))):
+        state.path.append(.Link(LinkFeature.State(linkType: .summaryCompleted(feedId: feedId))))
+        return .none
+                
+      // 요약완료 화면 Delegate
+      case let .path(.element(id: _, action: .Link(.delegate(.summaryCompletedSaveButtonTapped(feedId))))):
+        state.path.append(.Link(LinkFeature.State(linkType: .summarySave(feedId: feedId))))
+        return .none
+                
+      case .path(.element(id: _, action: .Link(.delegate(.summaryCompletedCloseButtonTapped)))):
+        state.path.removeAll()
+        /// 모달 띄우기
+        return .none
+        
+      // 요약완료 -> 요약완료 확인화면 Delegate
+      case .path(.element(id: _, action: .Link(.delegate(.summarySaveCloseButtonTapped)))):
+        state.path.removeAll()
+        /// homeView 리로드
         return .none
         
       default:
         return .none
       }
     }
-    .ifLet(\.$saveLink, action: \.saveLink) {
-      SaveLinkFeature()
-    }
+    .forEach(\.path, action: \.path)
   }
 }
