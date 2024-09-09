85d54ccc04060459599184b2bd487ddbeea08495
kimkyuchul <rbcjf122@gmail.com>
2024-09-09 11:09:38 +0900

    [CHORE] 링크 요약 시 response feedID (#78)

---
+++ Projects/Core/Services/Network/Foundation/Base/FeedIDResponse.swift
@@ -0,0 +1,13 @@
+//
+//  FeedIDResponse.swift
+//  Services
+//
+//  Created by kyuchul on 9/9/24.
+//  Copyright © 2024 com.kyuchul.blink. All rights reserved.
+//
+
+import Foundation
+
+struct FeedIDResponse: Decodable {
+  let feedId: Int
+}
