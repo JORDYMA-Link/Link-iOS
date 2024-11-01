d405cea8bac03a44c9049fd3f355609ae0c420d6
kimkyuchul <rbcjf122@gmail.com>
2024-11-01 14:03:49 +0900

    [ADD] Analytics 수집 데이터 Struct  (#130)

---
+++ Projects/Core/Analytics/Sources/AnalyticsyLogEvent.swift
@@ -0,0 +1,38 @@
+//
+//  AnalyticsyLogEvent.swift
+//  Analytics
+//
+//  Created by kyuchul on 11/1/24.
+//  Copyright © 2024 com.kyuchul.blink. All rights reserved.
+//
+
+import Foundation
+
+public struct AnalyticsLogEvent {
+  public let screen: AnalyticsScreenName
+  public let name: AnalyticsScreenName
+  public let extraParameters: [AnalyticsParameterName: Any]?
+  
+  public init(
+    screen: AnalyticsScreenName,
+    name: AnalyticsScreenName,
+    extraParameters: [AnalyticsParameterName : Any]?
+  ) {
+    self.screen = screen
+    self.name = name
+    self.extraParameters = extraParameters
+  }
+  
+  public var parameters: [String: Any] {
+    var parameters: [String:Any] = [:]
+    
+    if let extraParameters {
+      extraParameters.forEach { key, value in
+        parameters[key.rawValue] = value
+      }
+    }
+    
+    parameters["screen"] = screen.rawValue
+    return parameters
+  }
+}
