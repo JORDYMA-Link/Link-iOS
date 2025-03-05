44a01e6e13aa16566ce19efe464e2a9ce3c863ea
kimkyuchul <rbcjf122@gmail.com>
2025-03-05 16:02:28 +0900

    [ADD] BKWebViewAction (#188)

---
+++ Projects/Feature/Scene/WebView/BKWebViewAction.swift
@@ -0,0 +1,13 @@
+//
+//  BKWebViewAction.swift
+//  Feature
+//
+//  Created by kyuchul on 3/5/25.
+//  Copyright © 2025 com.kyuchul.blink. All rights reserved.
+//
+
+import Foundation
+
+enum BKWebViewAction {
+  case survey(SurveyJSPlugin.SurveyJSAction)
+}
