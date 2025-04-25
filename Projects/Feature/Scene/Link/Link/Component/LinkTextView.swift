c333f0e256c67b4aedee2b14d7a256a364f9f47b
김규철 <rbcjf122@gmail.com>
2025-04-25 15:42:46 +0900

    [CHORE] 링크 디테일 > TextView Expandable 기능 추가 (#198)

--- Projects/Feature/Scene/Link/Link/Component/LinkTextView.swift
+++ Projects/Feature/Scene/Link/Link/Component/LinkTextView.swift
@@ -12,6 +12,8 @@ import CommonFeature
 
 struct LinkTextView: View {
   private let content: String
+  @State private var contentHeight: CGFloat = 0
+  @State private var isExpandable: Bool = false
   
   init(
     content: String
@@ -20,17 +22,62 @@ struct LinkTextView: View {
   }
   
   var body: some View {
-    BKText(
-      text: content,
-      font: .regular,
-      size: ._14,
-      lineHeight: 20,
-      color: .bkColor(.gray800)
-    )
-    .frame(maxWidth: .infinity, alignment: .leading)
-    .multilineTextAlignment(.leading)
+    VStack(spacing: 10) {
+      BKText(
+        text: content,
+        font: .regular,
+        size: ._14,
+        lineHeight: 20,
+        color: .bkColor(.gray800)
+      )
+      .background(ViewHeightGeometry())
+      .onPreferenceChange(ViewPreferenceKey.self) { height in
+        DispatchQueue.main.async {
+          if !content.isEmpty && height > 0 {
+            contentHeight = height
+          }
+        }
+      }
+      .frame(maxWidth: .infinity, alignment: .leading)
+      .lineLimit(isExpandable && contentHeight > 60 ? nil : 3)
+      .multilineTextAlignment(.leading)
+      
+      if !isExpandable && contentHeight > 60 {
+        Divider()
+          .foregroundStyle(Color.bkColor(.gray400))
+          .frame(maxWidth: .infinity)
+        
+        LinkTextViewExpandButton(action: { isExpandable = true })
+      }
+    }
     .padding(EdgeInsets(top: 13, leading: 16, bottom: 13, trailing: 16))
     .background(Color.bkColor(.gray300))
     .clipShape(RoundedRectangle(cornerRadius: 10))
+    .animation(.default, value: isExpandable)
+  }
+}
+
+private struct LinkTextViewExpandButton: View {
+  private let action: () -> Void
+  
+  init(action: @escaping () -> Void) {
+    self.action = action
+  }
+  
+  var body: some View {
+    Button(action: action) {
+      HStack {
+        BKText(
+          text: "펼치기",
+          font: .regular,
+          size: ._14,
+          lineHeight: 20,
+          color: .bkColor(.gray800)
+        )
+        
+        CommonFeature.Images.icoChevronDown
+      }
+      .frame(maxWidth: .infinity, minHeight: 20, maxHeight: 20)
+    }
   }
 }
